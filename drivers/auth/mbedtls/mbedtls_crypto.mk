71dbf5fbc72d6486895f7d77a0732419114fa8d2
Konrad Adamczyk <konrad.adamczyk@cavium.com>
2017-07-03 12:09:41 +0200

    trust: Expand crypto makefile with TBBR_AES_128_CBC option used to decrypt image
    Change-Id: I7f36691482f8eca1dd2af175ae18ab28ece71676
    Signed-off-by: Konrad Adamczyk <konrad.adamczyk@cavium.com>

--- drivers/auth/mbedtls/mbedtls_crypto.mk
+++ drivers/auth/mbedtls/mbedtls_crypto.mk
@@ -8,4 +8,15 @@ include drivers/auth/mbedtls/mbedtls_common.mk
 
 MBEDTLS_SOURCES	+=		drivers/auth/mbedtls/mbedtls_crypto.c
 
-
+# Cipher type is used to encrypt/decrypt images
+ifeq (${CRYPTO_BOARD_BOOT},1)
+    ifeq (${MBEDTLS_CIPHER_TYPE},aes-128-cbc)
+        MBEDTLS_SOURCES	+=	$(addprefix ${MBEDTLS_DIR}/library/,	\
+				aes.c					\
+				)
+        TBBR_CIPHER_TYPE_ID	:=	TBBR_AES_128_CBC
+    else
+        $(error "MBEDTLS_CIPHER_TYPE=${MBEDTLS_CIPHER_TYPE} not supported on mbedTLS")
+    endif
+    $(eval $(call add_define,TBBR_CIPHER_TYPE_ID))
+endif
