bbd1ec0ad3ea37f019e45e133111c759628976c7
Tomasz Michalec <tomasz@marvell.com>
2019-01-04 12:52:03 +0100

    make_helpers: tbbr: Add support for OpenSSL engines in Makefile
    This patch add support for new option in cert_create tool to select
    OpenSSL engine. New build option is OPENSSL_ENGINE.
    
    Change-Id: I955b042a04c878317a81d3c8c4027a7df0c2fda4
    Signed-off-by: Tomasz Michalec <tomasz@marvell.com>
    Reviewed-on: https://sj1git1.cavium.com/6008
    Tested-by: sa_ip-sw-jenkins
    Reviewed-by: Nithya Mani <Nithya.Mani@cavium.com>
    Reviewed-by: Chandrakala Chavva <Chandrakala.Chavva@cavium.com>

--- make_helpers/tbbr/tbbr_tools.mk
+++ make_helpers/tbbr/tbbr_tools.mk
@@ -34,6 +34,7 @@
 #   BL31_KEY
 #   BL32_KEY
 #   BL33_KEY
+#   OPENSSL_ENGINE
 #
 
 # Certificate generation tool default parameters
@@ -80,6 +81,9 @@ $(if ${CORE_SWD_KEY},$(eval $(call CERT_ADD_CMD_OPT,${CORE_SWD_KEY},--core-swd-k
 $(if ${TRUSTED_WORLD_KEY},$(eval $(call CERT_ADD_CMD_OPT,${TRUSTED_WORLD_KEY},--trusted-world-key)))
 $(if ${NON_TRUSTED_WORLD_KEY},$(eval $(call CERT_ADD_CMD_OPT,${NON_TRUSTED_WORLD_KEY},--non-trusted-world-key)))
 
+# Select OpenSSL engine in the cert_create tool command line options
+$(if ${OPENSSL_ENGINE},$(eval $(call CERT_ADD_CMD_OPT,${OPENSSL_ENGINE},--engine)))
+$(if ${OPENSSL_ENGINE},$(eval $(call CERT_ADD_CMD_OPT,${OPENSSL_ENGINE},--engine,FWU_)))
 
 # Add the BL2 CoT (image cert)
 ifeq (${NEED_BL2},yes)
