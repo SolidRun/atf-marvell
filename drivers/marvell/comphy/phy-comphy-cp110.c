65f0b4d31630fedbf38486f3806a37c901abd531
Grzegorz Jaszczyk <jaz@semihalf.com>
2019-04-12 16:57:14 +0200

    marvell: comphy: start AP FW when comphy AP mode selected
    After configuring comphy to AP mode also start AP FW.
    
    Change-Id: I1542c118cb6342af6a30b6c3704fc6f4c5bd399a
    Signed-off-by: Grzegorz Jaszczyk <jaz@semihalf.com>
    Reviewed-on: https://sj1git1.cavium.com/18346
    Reviewed-by: Kostya Porotchkin <kostap@marvell.com>
    Tested-by: Kostya Porotchkin <kostap@marvell.com>

--- drivers/marvell/comphy/phy-comphy-cp110.c
+++ drivers/marvell/comphy/phy-comphy-cp110.c
@@ -2317,6 +2317,7 @@ static int mvebu_cp110_comphy_ap_power_on(uint64_t comphy_base,
 	reg_set(comphy_addr + COMMON_PHY_CFG1_REG, data, mask);
 	debug_exit();
 
+<<<<<<< HEAD
 #if MSS_SUPPORT
 	do {
 		uint8_t ap_nr, cp_nr;
@@ -2324,6 +2325,11 @@ static int mvebu_cp110_comphy_ap_power_on(uint64_t comphy_base,
 		/* start ap fw */
 		mvebu_cp110_get_ap_and_cp_nr(&ap_nr, &cp_nr, comphy_base);
 		mg_start_ap_fw(cp_nr, comphy_index);
+=======
+	/* start ap fw */
+	mvebu_cp110_get_ap_and_cp_nr(&ap_nr, &cp_nr, comphy_base);
+	mg_start_ap_fw(cp_nr);
+>>>>>>> marvell: comphy: start AP FW when comphy AP mode selected
 
 	} while (0);
 #endif
