d11f4640f1c89e73ee4ce60c7d26460d3a7470f1
Yan Markman <ymarkman@marvell.com>
2019-11-21 08:06:31 -0800

    octeontx2: mbedtls: fix makefiles used in secured builds
    Change-Id: If4c3648379c93e6f5c578a21451cbd14ecd8a1cf
    Signed-off-by: Yan Markman <ymarkman@marvell.com>
    Reviewed-on: https://sj1git1.cavium.com/19087
    Reviewed-by: Kostya Porotchkin <kostap@marvell.com>
    Tested-by: sa_ip-sw-jenkins <sa_ip-sw-jenkins@marvell.com>

--- drivers/auth/mbedtls/mbedtls_common.mk
+++ drivers/auth/mbedtls/mbedtls_common.mk
@@ -13,7 +13,8 @@ ifeq (${MBEDTLS_DIR},)
   $(error Error: MBEDTLS_DIR not set)
 endif
 
-MBEDTLS_INC		=	-I${MBEDTLS_DIR}/include
+INCLUDES		+=	-I${MBEDTLS_DIR}/include		\
+				-Iinclude/drivers/auth/mbedtls
 
 # Specify mbed TLS configuration file
 MBEDTLS_CONFIG_FILE	?=	"<drivers/auth/mbedtls/mbedtls_config.h>"
@@ -31,7 +32,6 @@ LIBMBEDTLS_SRCS		:= $(addprefix ${MBEDTLS_DIR}/library/,	\
 					memory_buffer_alloc.c			\
 					oid.c 					\
 					platform.c 				\
-					platform_util.c				\
 					bignum.c				\
 					gcm.c 					\
 					md.c					\
@@ -45,7 +45,6 @@ LIBMBEDTLS_SRCS		:= $(addprefix ${MBEDTLS_DIR}/library/,	\
 					ecp_curves.c				\
 					ecp.c					\
 					rsa.c					\
-					rsa_internal.c				\
 					x509.c 					\
 					x509_crt.c 				\
 					constant_time.c 			\
