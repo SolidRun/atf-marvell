4b4c2f94f28061d50f08ae31b3c5aa275d57b87c
Tomasz Michalec <tomasz@marvell.com>
2019-12-09 17:37:05 +0100

    plat: marvell: octeontx_ecam: Set only some domains in iobn5 init
    On platforms which has correctly implemented ECAMX_DOMX_CONST register
    there is no need to set IOBN registers for all domains. IOBN regsiters
    are set only for enabled ECAM domains.
    
    Change-Id: Ie78bfab022601dcb21ed1da31a65c7742192d681
    Signed-off-by: Tomasz Michalec <tomasz@marvell.com>
    Reviewed-on: https://sj1git1.cavium.com/20094
    Reviewed-by: Chandrakala Chavva <cchavva@marvell.com>
    Tested-by: Chandrakala Chavva <cchavva@marvell.com>

--- plat/marvell/octeontx/octeontx_ecam.c
+++ plat/marvell/octeontx/octeontx_ecam.c
@@ -407,7 +407,10 @@ static void init_iobn5(uint64_t config_base, uint64_t config_size)
 	int i, iobn_nr;
 	int domain, bus, dev;
 	union cavm_pccpf_xxx_vsec_ctl vsec_ctl;
-	int iobn5_domain_max = 0x7;
+	union cavm_ecamx_const ecamx_const;
+	union cavm_ecamx_domx_const domx_const;
+	int iobn5_domain_max;
+	int set_all_domains;
 
 	vsec_ctl.u = octeontx_read32(config_base + CAVM_PCCPF_XXX_VSEC_CTL);
 	iobn_nr = vsec_ctl.s.inst_num;
@@ -425,12 +428,31 @@ static void init_iobn5(uint64_t config_base, uint64_t config_size)
 		CSR_WRITE(CAVM_IOBNX_RSLX_STREAMS(iobn_nr,i), 0x3);
 	}
 
-	/*
-	 * ECAMX_DOMX_CONST shows up only that are present under it.
-	 * To enable PEMX controllers on domain 3 4 5 6, use the loop max
-	 * as 0x7 to include them.
-	 */
+	if (IS_OCTEONTX_VAR(read_midr(), T96PARTNUM, 1) ||
+	    IS_OCTEONTX_PN(read_midr(), F95PARTNUM) ||
+	    IS_OCTEONTX_PN(read_midr(), F95MMPARTNUM)) {
+		/*
+		 * ECAMX_DOMX_CONST shows up only that are present under it.
+		 * To enable PEMX controllers on domain 3 4 5 6, use the loop
+		 * max as 0x7 to include them.
+		 */
+		iobn5_domain_max = 0x7;
+		set_all_domains = 1;
+	} else {
+		ecamx_const.u = CSR_READ(CAVM_ECAMX_CONST(0));
+		iobn5_domain_max = ecamx_const.s.domains;
+		set_all_domains = 0;
+	}
+
 	for (domain = 0; domain < iobn5_domain_max; domain++) {
+		if (!set_all_domains) {
+			/* Domains may not be contiguous */
+			domx_const.u = CSR_READ(CAVM_ECAMX_DOMX_CONST(
+								0, domain));
+			if (!domx_const.s.pres)
+				continue;
+		}
+
 		for (bus = 0; bus < 256; bus++) {
 			/* To get secure interrupts for GPIO and BPHY
 			 * the PHYS_NSEC has to be 0
