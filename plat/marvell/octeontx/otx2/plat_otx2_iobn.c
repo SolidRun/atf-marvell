3a164ead06d088736bdd924476019e91bbd64e39
Rick Farrington <Ricardo.Farrington@cavium.com>
2020-03-05 14:52:28 -0500

    plat: marvell: otx2: use table-driven IOBN stream security settings
    No functional changes were made in the IOBN initialization; only
    code re-organization was performed.
    
    Change-Id: I587549d19194db8f93af5ea8df72b3f6a00b2962
    Signed-off-by: Rick Farrington <Ricardo.Farrington@cavium.com>
    Reviewed-on: https://sj1git1.cavium.com/24547
    Tested-by: sa_ip-sw-jenkins <sa_ip-sw-jenkins@marvell.com>
    Reviewed-by: Chandrakala Chavva <cchavva@marvell.com>

--- plat/marvell/octeontx/otx2/plat_otx2_iobn.c
+++ plat/marvell/octeontx/otx2/plat_otx2_iobn.c
@@ -20,25 +20,106 @@
 #include "cavm-csrs-iobn.h"
 #include "cavm-csrs-pccpf.h"
 
+/*
+ * stream_uses_rslx_or_devx()
+ *
+ * This is used to indicate if a particular stream is affected by
+ * IOBNX_RSLX_STREAMS or IOBNX_DOMX_DEVX_STREAMS.
+ *
+ * When programming IOBN security permissions, streams use a combination of
+ * IOBNX_DOMX_BUSX_STREAMS and one of:
+ *   IOBNX_DOMX_DEVX_STREAMS
+ *   IOBNX_RSLX_STREAMS
+ *
+ * See HRM documentation for 'IOBNX_DOMX_BUSX_STREAMS'.
+ *
+ * This function helps distinguish the correct registers to use for
+ * security permissions.
+ *
+ * on entry,
+ *   streamid : one of 'CAVM_PCC_DEV_CON_E_xxx'
+ *
+ * returns,
+ *   false  if specified streamid is affected by IOBNX_DOMX_DEVX_STREAMS
+ *   !false if specified streamid is affected by IOBNX_RSLX_STREAMS
+ */
+static bool stream_uses_rslx_or_devx(uint32_t streamid)
+{
+	int domain_idx, bus_idx;
+
+	domain_idx = STREAM_DMN_IDX(streamid);
+	bus_idx = STREAM_BUS_IDX(streamid);
+
+	/* Streams on same domain & bus as CAVM_PCC_DEV_CON_E_MRML are
+	 * affected by IOBNX_RSLX_STREAMS.
+	 */
+	return (domain_idx == STREAM_DMN_IDX(CAVM_PCC_DEV_CON_E_MRML)) &&
+	       (bus_idx == STREAM_BUS_IDX(CAVM_PCC_DEV_CON_E_MRML));
+}
+
 void octeontx_init_iobn(uint64_t config_base, uint64_t config_size)
 {
-	int i, iobn_nr;
-	int domain, bus, dev;
+	int rsl_idx, iobn_nr;
+	int domain, bus, dev, j, inst;
 	union cavm_pccpf_xxx_vsec_ctl vsec_ctl;
 	union cavm_ecamx_const ecamx_const;
 	union cavm_ecamx_domx_const domx_const;
 	int iobn5_domain_max;
 	int set_all_domains;
+	int strm_nsec, phys_nsec;
+	uint32_t streamid;
+	/*
+	 * Settings in this table are common to all OTX2 platforms.
+	 *
+	 * This array contains specific permissions settings for
+	 * individual streams (i.e. devices).
+	 *
+	 * All other settings use the default (i.e. non-secure):
+	 *   strm_nsec = 1
+	 *   phys_nsec = 1
+	 */
+	struct otx2_stream_security_setting stream_settings[] = {
+		/* To get secure interrupts for GPIO, PHYS_NSEC must == 0 */
+		{ CAVM_PCC_DEV_CON_E_GPIO_CN9, 1, 1 /* strm */, 0 /* phys */ },
+		/* To get secure interrupts for BPHY, PHYS_NSEC must == 0 */
+		{ CAVM_PCC_DEV_CON_E_BPHY, 1, 1 /* strm */, 0 /* phys */ },
+	};
 
 	vsec_ctl.u = octeontx_read32(config_base + CAVM_PCCPF_XXX_VSEC_CTL);
 	iobn_nr = vsec_ctl.s.inst_num;
 
 	/*
 	 * Allow all IO units to access non-secure memory.
-	 * We can program secure devices later when they discovered.
+	 * Program secure devices individually as needed
+	 * (per 'stream_settings[]' above).
 	 */
-	for (i = 0; i < 256; i++) {
-		CSR_WRITE(CAVM_IOBNX_RSLX_STREAMS(iobn_nr, i), 0x3);
+	for (rsl_idx = 0; rsl_idx < 256; rsl_idx++) {
+		CSR_WRITE(CAVM_IOBNX_RSLX_STREAMS(iobn_nr, rsl_idx), 0x3);
+
+		/* check if individual streams require RSLX setting */
+		for (j = 0; j < ARRAY_SIZE(stream_settings); j++) {
+			/* Iterate all instances of the stream */
+			for (inst = 0; inst < stream_settings[j].instances;
+			     inst++) {
+				streamid = stream_settings[j].streamid + inst;
+
+				/* A stream will use DEVX or RSLX, not both */
+				if (!stream_uses_rslx_or_devx(streamid))
+					continue;
+				/* Verify stream uses this RSLX register */
+				if (STREAM_RSL_IDX(streamid) != rsl_idx)
+					continue;
+
+				/* configure security in IOBNX_RSLX_STREAMS */
+				CSR_MODIFY(c,
+					CAVM_IOBNX_RSLX_STREAMS(iobn_nr,
+								rsl_idx),
+					c.s.strm_nsec =
+						stream_settings[j].strm_nsec;
+					c.s.phys_nsec =
+						stream_settings[j].phys_nsec);
+			}
+		}
 	}
 
 	if (IS_OCTEONTX_VAR(read_midr(), T96PARTNUM, 1) ||
@@ -67,28 +148,91 @@ void octeontx_init_iobn(uint64_t config_base, uint64_t config_size)
 		}
 
 		for (bus = 0; bus < 256; bus++) {
-			/* To get secure interrupts for GPIO and BPHY
-			 * the PHYS_NSEC has to be 0
-			 */
-			if (domain == 0 && (bus == 0 || bus == 6))
-				CSR_WRITE(CAVM_IOBNX_DOMX_BUSX_STREAMS(
-					  iobn_nr, domain, bus), 0x2);
-			else
-				CSR_WRITE(CAVM_IOBNX_DOMX_BUSX_STREAMS(
-					  iobn_nr, domain, bus), 0x3);
+			/* default setting */
+			CSR_WRITE(CAVM_IOBNX_DOMX_BUSX_STREAMS(
+				  iobn_nr, domain, bus), 0x3);
+
+			/* check if individual streams require BUSX setting */
+			for (j = 0; j < ARRAY_SIZE(stream_settings); j++) {
+				strm_nsec = stream_settings[j].strm_nsec;
+				phys_nsec = stream_settings[j].phys_nsec;
+
+				/* Iterate all instances of the stream */
+				for (inst = 0; inst <
+				     stream_settings[j].instances;
+				     inst++) {
+					streamid = stream_settings[j].streamid;
+					streamid += inst;
+
+					/* Verify stream uses this domain/bus */
+					if ((STREAM_DMN_IDX(streamid) !=
+					    domain))
+						continue;
+					if ((STREAM_BUS_IDX(streamid) != bus))
+						continue;
+
+					/* configure IOBNX_BUSX_STREAMS */
+					CSR_MODIFY(c,
+						CAVM_IOBNX_DOMX_BUSX_STREAMS(
+							iobn_nr, domain, bus),
+					/*
+					 * Only set a more-permissive
+					 * value. If already set to
+					 * be permissive (i.e. 0),
+					 * another device on this bus
+					 * has already been configured.
+					 * Don't disrupt its settings.
+					 * Refer to [96xx] HRM descr
+					 * of this register.
+					 */
+					if (c.s.strm_nsec == 1)
+						c.s.strm_nsec =
+							strm_nsec;
+					if (c.s.phys_nsec == 1)
+						c.s.phys_nsec =
+							phys_nsec
+					); /* end of CSR_MODIFY */
+				}
+			}
 		}
+
 		for (dev = 0; dev < 32; dev++) {
-			/* To get secure interrupts for GPIO and BPHY
-			 * the PHYS_NSEC has to be 0
-			 */
-			if (domain == 0 &&
-			    (dev == (CAVM_PCC_DEV_CON_E_GPIO_CN9 >> 3)
-			    || dev == ((CAVM_PCC_DEV_CON_E_BPHY & 0xFF) >> 3)))
-				CSR_WRITE(CAVM_IOBNX_DOMX_DEVX_STREAMS(
-					  iobn_nr, domain, dev), 0x2);
-			else
-				CSR_WRITE(CAVM_IOBNX_DOMX_DEVX_STREAMS(
-					  iobn_nr, domain, dev), 0x3);
+			/* default setting */
+			CSR_WRITE(CAVM_IOBNX_DOMX_DEVX_STREAMS(
+				  iobn_nr, domain, dev), 0x3);
+
+			/* check if individual streams require DEVX setting */
+			for (j = 0; j < ARRAY_SIZE(stream_settings); j++) {
+				strm_nsec = stream_settings[j].strm_nsec;
+				phys_nsec = stream_settings[j].phys_nsec;
+
+				/* Iterate all instances of the stream */
+				for (inst = 0; inst <
+				     stream_settings[j].instances;
+				     inst++) {
+					streamid = stream_settings[j].streamid;
+					streamid += inst;
+
+					/* stream uses DEVX or RSLX, not both */
+					if (stream_uses_rslx_or_devx(streamid))
+						continue;
+					/* verify stream uses this domain/dev */
+					if ((STREAM_DMN_IDX(streamid) !=
+					    domain))
+						continue;
+					if ((STREAM_DEV_IDX(streamid) != dev))
+						continue;
+
+					/* configure IOBNX_DEVX_STREAMS */
+					CSR_MODIFY(c,
+						CAVM_IOBNX_DOMX_DEVX_STREAMS(
+							iobn_nr, domain, dev),
+						c.s.strm_nsec = strm_nsec;
+						c.s.phys_nsec = phys_nsec);
+				}
+			}
 		}
 	}
 }
+
+
