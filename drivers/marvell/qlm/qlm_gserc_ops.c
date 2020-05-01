5a2128e242b9aa053f5045f2909277e3cfd949f0
Nithya Mani <nithya.mani@cavium.com>
2020-05-01 16:16:39 -0700

    drivers: marvell: gserc: Add RAMBUS AN/LT support to GSERC
    Rambus AN/LT support APIs are added to GSERC driver
    similar to GSERR.
    
    Change-Id: I21100fdb44e3f6612f3ed5d77cb72859409d7f55
    Signed-off-by: Nithya Mani <nithya.mani@cavium.com>
    Reviewed-on: https://sj1git1.cavium.com/c/IP/SW/boot/atf/+/27777
    Tested-by: Chandrakala Chavva <cchavva@marvell.com>
    Reviewed-by: Chandrakala Chavva <cchavva@marvell.com>
    Signed-off-by: Rick Farrington <rfarrington@marvell.com>

--- drivers/marvell/qlm/qlm_gserc_ops.c
+++ drivers/marvell/qlm/qlm_gserc_ops.c
@@ -283,6 +283,99 @@ static int qlm_gserc_link_training_complete(int qlm, int lane)
 }
 
 /**
+ * Set GSERC handshake bit to start AN
+ *
+ * @param  qlm    Index into GSER* group
+ * @param lane       Which lane
+ */
+static void qlm_gserc_start_an(int qlm, int lane)
+{
+	/* AN Handshaking bit assignments
+	 * GSERCX_LNX_FEATURE_SPARE_CFG6_RSVD[data]
+	 * data[3] = Handshake enable
+	 * data[4] = Handshake ready
+	 */
+	GSER_CSR_MODIFY(c, CAVM_GSERCX_LNX_FEATURE_SPARE_CFG6_RSVD(qlm, lane),
+				c.s.data &= 0xf7);
+}
+
+/**
+ * Set Phy Strap
+ *
+ * @param qlm    Index into GSER* group
+ * @param lane      Which lane
+ */
+static void qlm_gserc_set_phy_strap(int qlm, int lane)
+{
+	/* Read GSERCX_LANEX_CONTROL_BCFG Fields. Read initially
+	 * from GSERCX_LANEX_STATUS_BSTS [ln_an_link_sel]
+	 */
+	GSER_CSR_INIT(status_bsts, CAVM_GSERCX_LANEX_STATUS_BSTS(qlm, lane));
+	/* Write value from above into GSERCX_LANEX_CONTROL_BCFG [ln_link_stat] */
+	GSER_CSR_MODIFY(control_bcfg, CAVM_GSERCX_LANEX_CONTROL_BCFG(qlm, lane),
+		control_bcfg.s.ln_link_stat = status_bsts.s.ln_an_link_sel);
+}
+
+/**
+ * Check whether SERDES AN Completed
+ *
+ * @param qlm	  QLM to use
+ * @param lane	  Which lane
+ * @return 0 on completion, 1 not complete
+ */
+static int qlm_gserc_an_complete(int qlm, int lane)
+{
+	GSER_CSR_INIT(lane_status, CAVM_GSERCX_LANEX_STATUS_BSTS(qlm, lane));
+	if (lane_status.s.ln_an_stat_resolved)
+		return 0;
+	else
+		return 1;
+}
+
+/**
+ * Read CSRs to determine Link Training Status
+ *
+ * @param  qlm   Index into GSER* group
+ * @param  lane    Which lane
+ */
+static void qlm_gserc_get_link_training_status(int qlm, int lane)
+{
+#ifdef DEBUG_ATF_GSER
+	GSER_CSR_INIT(fsm_ctrl5, CAVM_GSERCX_LNX_LT_TX_FSM_CTRL5(qlm, lane));
+	GSER_TRACE(QLM, "GSERC%d_LN%d_LT_TX_FSM_CTRL5\n", qlm, lane);
+			GSER_TRACE(QLM, "remote_rx_ready: %d\trx_trained: %d\n",
+			fsm_ctrl5.s.remote_rx_ready,
+			fsm_ctrl5.s.rx_trained);
+
+	GSER_CSR_INIT(fsm_status, CAVM_GSERCX_LNX_LT_TX_FSM_STATUS(qlm, lane));
+	GSER_TRACE(QLM, "GSERC%d_LN%d_LT_TX_FSM_STATUS\n", qlm, lane);
+	GSER_TRACE(QLM, "fsm_local_rx_ready: %d\tsignal_detect: %d\ttraining_fail: %d\n",
+			fsm_status.s.fsm_local_rx_ready,
+			fsm_status.s.signal_detect,
+			fsm_status.s.training_fail);
+
+	GSER_CSR_INIT(fsm_state_status0, CAVM_GSERCX_LNX_LT_TX_FSM_STATE_STATUS0(qlm, lane));
+	GSER_TRACE(QLM, "GSERC%d_LN%d_LT_TX_FSM_STATE_STATUS0\n", qlm, lane);
+	GSER_TRACE(QLM, "prev1: %d\tcurrent: %d\n",
+			fsm_state_status0.s.prev1,
+			fsm_state_status0.s.current);
+
+	GSER_CSR_INIT(fsm_state_status1, CAVM_GSERCX_LNX_LT_TX_FSM_STATE_STATUS1(qlm, lane));
+	GSER_TRACE(QLM, "GSERC%d_LN%d_LT_TX_FSM_STATE_STATUS1\n", qlm, lane);
+	GSER_TRACE(QLM, "prev2: %d\tprev3: %d\n",
+			fsm_state_status1.s.prev2,
+			fsm_state_status1.s.prev3);
+#endif
+}
+
+static void qlm_gserc_clear_link_stat(int qlm, int lane)
+{
+	GSER_CSR_MODIFY(control_bcfg, CAVM_GSERCX_LANEX_CONTROL_BCFG(
+		qlm, lane),
+		control_bcfg.s.ln_link_stat = 0);
+}
+
+/**
  * Check if PRBS Rx or Tx is enabled
  *
  * @param  qlm        Index into GSER* group
@@ -325,5 +418,10 @@ const qlm_ops_t qlm_gserc_ops = {
 	.qlm_link_training_config = qlm_gserc_link_training_config,
 	.qlm_link_training_fail = qlm_gserc_link_training_fail,
 	.qlm_link_training_complete = qlm_gserc_link_training_complete,
+	.qlm_start_an = qlm_gserc_start_an,
+	.qlm_set_phy_strap = qlm_gserc_set_phy_strap,
+	.qlm_an_complete = qlm_gserc_an_complete,
+	.qlm_get_link_training_status = qlm_gserc_get_link_training_status,
+	.qlm_clear_link_stat = qlm_gserc_clear_link_stat,
 	.qlm_prbs_chk = qlm_gserc_prbs_chk,
 };
