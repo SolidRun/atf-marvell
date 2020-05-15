ae258b95219ae4268b733d98ce1f1cbba62a6fa0
Chandrakala Chavva <Chandrakala.Chavva@cavium.com>
2020-05-14 17:45:42 -0700

    EHF: t83: workaround for bug (GIC returns Idle priority)
    Change-Id: I92d501441017beb5b32276059940af2ff631d589
    Signed-off-by: Chandrakala Chavva <Chandrakala.Chavva@cavium.com>
    Reviewed-on: https://sj1git1.cavium.com/c/IP/SW/boot/atf/+/28526
    Tested-by: Chandrakala Chavva <cchavva@marvell.com>
    Reviewed-by: Chandrakala Chavva <cchavva@marvell.com>
    Signed-off-by: Rick Farrington <rfarrington@marvell.com>

--- plat/common/plat_gicv3.c
+++ plat/common/plat_gicv3.c
@@ -111,7 +111,7 @@ uint32_t plat_ic_get_pending_interrupt_type(void)
 uint32_t plat_ic_acknowledge_interrupt(void)
 {
 #if (defined(PLAT_f95) || defined(PLAT_t96) || (PLAT_loki) || \
-	defined(PLAT_t98) || defined(PLAT_f95mm))
+	defined(PLAT_t98) || defined(PLAT_f95mm) || defined(PLAT_t83))
 #	define IAR_RETRIES 1
 #	define GIC_IDLE_PRIORITY 0xff
 	uint64_t icc_ap0r0_el1[2];
