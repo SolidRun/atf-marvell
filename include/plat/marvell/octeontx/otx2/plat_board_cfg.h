15f2622d22a8c4bf4441e9ab81c70874307f1e43
Rick Farrington <rfarrington@marvell.com>
2020-05-22 13:54:50 -0400

    Revert "plat: marvell: rvu: otx2: add support for SW_RVU_REE device"
    REE device support requires additional work.
    
    This reverts commit 66dabaad0d26c14fadf48b1424e29923ab33d939.
    
    Change-Id: Ice969048f5c92cf01acef8959ee4295343fe2c71
    Signed-off-by: Rick Farrington <rfarrington@marvell.com>
    Reviewed-on: https://sj1git1.cavium.com/c/IP/SW/boot/atf/+/28834
    Reviewed-by: Chandrakala Chavva <cchavva@marvell.com>
    Tested-by: Chandrakala Chavva <cchavva@marvell.com>
    Signed-off-by: Rick Farrington <rfarrington@marvell.com>

--- include/plat/marvell/octeontx/otx2/plat_board_cfg.h
+++ include/plat/marvell/octeontx/otx2/plat_board_cfg.h
@@ -53,11 +53,6 @@ typedef struct rvu_sw_rvu_pf {
 #endif
 
 /* default, if not defined by platform */
-#ifndef SW_RVU_REE_NUM_PF
-#define SW_RVU_REE_NUM_PF     0
-#endif
-
-/* default, if not defined in platform */
 #ifndef SW_RVU_CPT_NUM_PF
 #define SW_RVU_CPT_NUM_PF     1
 #endif
@@ -68,7 +63,6 @@ typedef enum {
 	SW_RVU_NPA_BASE = SW_RVU_SSO_TIM_BASE + SW_RVU_SSO_TIM_NUM_PF,
 	SW_RVU_SDP_BASE = SW_RVU_NPA_BASE + SW_RVU_NPA_NUM_PF,
 	SW_RVU_CPT_BASE = SW_RVU_SDP_BASE + SW_RVU_SDP_NUM_PF,
-	SW_RVU_REE_BASE = SW_RVU_CPT_BASE + SW_RVU_CPT_NUM_PF,
 } sw_rvu_pfs;
 
 /*
@@ -85,14 +79,11 @@ typedef enum {
 			      SW_RVU_SDP_BASE + (n))
 #define SW_RVU_CPT_PF(n)     (!SW_RVU_CPT_NUM_PF ? SW_RVU_NODEV : \
 			      SW_RVU_CPT_BASE + (n))
-#define SW_RVU_REE_PF(n)     (!SW_RVU_REE_NUM_PF ? SW_RVU_NODEV : \
-			      SW_RVU_REE_BASE + (n))
 
 #define SW_RVU_NUM_PF        (SW_RVU_SSO_TIM_NUM_PF \
 			      + SW_RVU_NPA_NUM_PF \
 			      + SW_RVU_SDP_NUM_PF \
 			      + SW_RVU_CPT_NUM_PF \
-			      + SW_RVU_REE_NUM_PF \
 			      + 1 /* for SW_RVU_NODEV */)
 
 typedef struct rvu_config {
