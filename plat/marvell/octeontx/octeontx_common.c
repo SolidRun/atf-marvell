6c290fcea6e3a64fa2a4c7a189b17018262ad8fd
Rick Farrington <rfarrington@marvell.com>
2020-07-13 18:25:09 -0400

    plat: marvell: octeontx: maximize OS persistent store sub-regions
    For Linux 'ramoops' area, adjust size of 'pmsg-size' to maximum
    available memory after accounting for other sub-regions.
    
    Change-Id: Ie0aff60a0e6d8567a2cb9bfad1fbfcdb9eca3391
    Signed-off-by: Rick Farrington <rfarrington@marvell.com>
    Reviewed-on: https://sj1git1.cavium.com/c/IP/SW/boot/atf/+/31852
    Reviewed-by: Chandrakala Chavva <cchavva@marvell.com>
    Tested-by: sa_ip-sw-jenkins <sa_ip-sw-jenkins@marvell.com>
    Signed-off-by: Rick Farrington <rfarrington@marvell.com>

--- plat/marvell/octeontx/octeontx_common.c
+++ plat/marvell/octeontx/octeontx_common.c
@@ -23,6 +23,8 @@
 
 extern void plat_add_mmio();
 
+static void plat_initialize_os_persistent_area(void);
+
 void *fdt_ptr = (void *)~0;
 
 unsigned long plat_get_ns_image_entrypoint(void)
@@ -71,6 +73,12 @@ static void plat_add_mmio_common(void)
 #endif
 
 	plat_initialize_boot_error_data_area(attr);
+	/*
+	 * If appropriate, adjust OS persistent area Device Tree settings here,
+	 * before enabling mmu.  Once mmu has been enabled, the Device Tree is
+	 * read-only (see mapping of fdt_ptr above).
+	 */
+	plat_initialize_os_persistent_area();
 }
 
 void plat_add_mmio_map()
@@ -102,3 +110,87 @@ void plat_initialize_boot_error_data_area(unsigned long attr)
 {
 	(void)attr;
 }
+
+/*
+ * plat_initialize_os_persistent_area()
+ *
+ * Update the device tree settings for the OS persistent store area (ramoops).
+ * The device tree contains settings for sub-regions within this area.
+ * Adjust the size of the 'pmsg-size' area to use the maximum size available,
+ * after accounting for the sizes of the other sub-regions.
+ *
+ */
+void plat_initialize_os_persistent_area(void)
+{
+	uint32_t rec_sz, pmsg_sz, cons_sz, ftrace_sz, *resize_val, new_size;
+	uint64_t oops_base, oops_size, size, remaining;
+	const char *resize_prop = NULL;
+	const void *fdt = fdt_ptr;
+	const fdt64_t *freg64;
+	const fdt32_t *freg32;
+	fdt32_t dt_reg32;
+	int oops_off;
+
+	oops_off = fdt_path_offset(fdt, "/reserved-memory/ramoops");
+	if (oops_off < 0)
+		return;
+
+	/* Retrieve RAMOOPS DT settings */
+	oops_base = oops_size = 0;
+	freg64 = fdt_getprop(fdt, oops_off, "reg", NULL);
+	if (freg64) {
+		oops_base = fdt64p_to_cpu(&freg64[0]);
+		oops_size = fdt64p_to_cpu(&freg64[1]);
+	}
+
+	if (!oops_base || !oops_size)
+		return;
+
+	rec_sz = pmsg_sz = cons_sz = ftrace_sz = 0;
+	freg32 = fdt_getprop(fdt, oops_off, "pmsg-size", NULL);
+	if (freg32)
+		pmsg_sz = fdt32_to_cpu(*freg32);
+
+	/* if not allocated, there is nothing to resize */
+	if (!pmsg_sz)
+		return;
+
+	freg32 = fdt_getprop(fdt, oops_off, "record-size", NULL);
+	if (freg32)
+		rec_sz = fdt32_to_cpu(*freg32);
+	freg32 = fdt_getprop(fdt, oops_off, "console-size", NULL);
+	if (freg32)
+		cons_sz = fdt32_to_cpu(*freg32);
+	freg32 = fdt_getprop(fdt, oops_off, "ftrace-size", NULL);
+	if (freg32)
+		ftrace_sz = fdt32_to_cpu(*freg32);
+
+	/* ensure that sum of all regions does not exceed allocated size */
+	size = rec_sz + pmsg_sz + cons_sz + ftrace_sz;
+	if (size > oops_size) {
+		WARN("ramoops regions exceed allocated size\n");
+		return;
+	}
+
+	/* name of property to be resized to maximum */
+	resize_prop = "pmsg-size";
+	resize_val = &pmsg_sz;
+
+	/* based on total size, calculate unused (i.e. remaining) size */
+	remaining = oops_size - size;
+
+	/* determine new size of desired property */
+	size = (uint64_t)*resize_val + remaining;
+	/* properties are limited to 32-bits; use def value if limit exceeded */
+	if (size > (uint64_t)(uint32_t)-1)
+		size = 0x1000;
+
+	/* write resized property value back to device tree */
+	new_size = (uint32_t)size;
+	dt_reg32 = cpu_to_fdt32(new_size);
+	if (fdt_setprop((void *)fdt, oops_off, resize_prop, &dt_reg32,
+	    sizeof(dt_reg32)))
+		WARN("Cannot resize FDT property '%s' from 0x%x to 0x%x\n",
+		     resize_prop, *resize_val, new_size);
+}
+
