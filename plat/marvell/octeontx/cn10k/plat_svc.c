21e33c5fc9ef9511b8e4d5ac683fef6dfc5250e5
Chandrakala Chavva <Chandrakala.Chavva@cavium.com>
2020-07-22 16:33:08 -0700

    Revert "SPI: Add support to update firmware on Secure SPI"
    This reverts commit 8495383eb23096a9b6626cb616832d0c91e9e1b2.
    
    Change-Id: I2cc926a1c58db36f1d0a652e792e8da6ee7c28f4
    Reviewed-on: https://sj1git1.cavium.com/c/IP/SW/boot/atf/+/32513
    Tested-by: Chandrakala Chavva <cchavva@marvell.com>
    Reviewed-by: Chandrakala Chavva <cchavva@marvell.com>

--- plat/marvell/octeontx/cn10k/plat_svc.c
+++ plat/marvell/octeontx/cn10k/plat_svc.c
@@ -16,8 +16,6 @@
 #include <plat_board_cfg.h>
 #include <plat_scmi.h>
 #include <cgx.h>
-#include <spi.h>
-#include <octeontx_dram.h>
 
 extern void *scmi_handle;
 
@@ -43,48 +41,13 @@ uintptr_t plat_octeontx_svc_smc_handler(uint32_t smc_fid,
 					void *handle,
 					u_register_t flags)
 {
-	uintptr_t size, user_buf;
-	uint64_t bus = 0, cs = 0, dram_end = 0;
-	int ret = 0;
+	uint64_t ret = 0;
 
 	switch (smc_fid) {
 	case PLAT_OCTEONTX_DISABLE_RVU_LFS:
 		ret = octeontx_clear_lf_to_pf_mapping();
 		SMC_RET1(handle, ret);
 		break;
-
-	case PLAT_OCTEONTX_SPI_SECURE_UPDATE:
-		user_buf = x1;
-		size = x2;
-		bus = x3;
-		cs = x4;
-
-		/* Check if NS user_buf is a valid DRAM address */
-		if (NULL == (void *)user_buf) {
-			ret = -1;
-			goto err;
-		}
-
-		dram_end = octeontx_dram_size();
-		/* Sanity check */
-		if ((user_buf < NS_IMAGE_BASE) ||
-			(user_buf > dram_end - 1) ||
-			(bus > MAX_SPI_BUS) ||
-			(cs > MAX_SPI_CS) ||
-			(size == 0) ||
-			(plat_octeontx_bcfg->spi_cfg[bus].is_secure != 1)
-			(plat_octeontx_bcfg->spi_cfg[bus].cs[cs] != 1)) {
-			WARN("secure_spi: Invalid parameters\n");
-			ret = -1;
-			goto err;
-		}
-
-		/* Perform SPI update */
-		ret = spi_secure_update(user_buf, size, bus, cs);
-err:
-		SMC_RET1(handle, ret);
-		break;
-
 	default:
 		return cn10k_svc_smc_handler(smc_fid, x1, x2, x3, x4,
 					    cookie, handle, flags);
