e1bdc3af73c4d26d3238153ec8c4ccb276dc6170
Jayanthi A <jayanthi.annadurai@cavium.com>
2020-11-19 17:14:18 -0800

    plat: marvell: cn10k: SVC handler for cnf10ka and cnf10kb
    Change-Id: I6250993b9c4c23350ebda6a10af14cd724b6a9bb
    Signed-off-by: Jayanthi A <jayanthi.annadurai@cavium.com>
    Reviewed-on: https://sj1git1.cavium.com/c/IP/SW/boot/atf/+/40516
    Reviewed-by: Chandrakala Chavva <cchavva@marvell.com>
    Tested-by: Chandrakala Chavva <cchavva@marvell.com>

---
+++ plat/marvell/octeontx/cn10k/cnf10kb/plat_cnf10kb_svc.c
@@ -0,0 +1,70 @@
+/*
+ * Copyright (C) 2020 Marvell.
+ *
+ * SPDX-License-Identifier:     BSD-3-Clause
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ * this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ *
+ * 3. Neither the name of the copyright holder nor the names of its
+ * contributors may be used to endorse or promote products derived from this
+ * software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+
+#include <debug.h>
+#include <debug.h>
+#include <runtime_svc.h>
+#include <octeontx_svc.h>
+#include <platform_svc.h>
+#include <octeontx_common.h>
+#include <stdint.h>
+#include <tools_share/uuid.h>
+#include <bphy.h>
+
+uintptr_t cn10k_svc_smc_handler(uint32_t smc_fid,
+			       u_register_t x1,
+			       u_register_t x2,
+			       u_register_t x3,
+			       u_register_t x4,
+			       void *cookie,
+			       void *handle,
+			       u_register_t flags)
+{
+	uint64_t ret = 0;
+
+	switch (smc_fid) {
+	case PLAT_OCTEONTX_INSTALL_BPHY_PSM_ERRINT:
+		ret = bphy_psm_install_irq(x1, x2, x3, x4);
+		SMC_RET1(handle, ret);
+		break;
+
+	case PLAT_OCTEONTX_REMOVE_BPHY_PSM_ERRINT:
+		bphy_psm_clear_irq(x1);
+		SMC_RET1(handle, 0);
+		break;
+
+	default:
+		WARN("Unimplemented OcteonTX CNF10Kb Service Call: 0x%x\n", smc_fid);
+		SMC_RET1(handle, SMC_UNK);
+	}
+}
