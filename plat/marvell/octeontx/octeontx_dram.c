73d4bd8de6ea437dd20f10a425cd579e73a0d84c
Chandrakala Chavva <Chandrakala.Chavva@cavium.com>
2020-11-21 15:56:58 -0800

    Revert "plat: marvell: Fix to return total dram size"
    This reverts commit a290281e54f3d529306e7875076cbc989b72846a.
    
    integration references:
      https://sj1git1.cavium.com/c/IP/SW/boot/atf/+/40657
    
    Change-Id: I31703757ab93a70624d4e95f1a0aca053100ad00
    Signed-off-by: Rick Farrington <rfarrington@marvell.com>
    Reviewed-on: https://sj1git1.cavium.com/c/IP/SW/boot/atf/+/43208
    Reviewed-by: Chandrakala Chavva <cchavva@marvell.com>
    Tested-by: Chandrakala Chavva <cchavva@marvell.com>

--- plat/marvell/octeontx/octeontx_dram.c
+++ plat/marvell/octeontx/octeontx_dram.c
@@ -43,15 +43,11 @@ static inline uint32_t popcnt(uint64_t val)
 	return x;  /* (7 significant bits) */
 }
 
-/* This function returns the size of DRAM, which includes
- * SECURE_NONPRESERVE and NSECURE_NONPRESERVE memory region
- */
 uint64_t octeontx_dram_size()
 {
-	uint64_t size, addr = 0;
+	uint64_t addr = 0;
 
-	size = memory_region_get_info(NSECURE_NONPRESERVE, &addr);
-	return (size + addr);
+	return memory_region_get_info(NSECURE_NONPRESERVE, &addr);
 }
 
 uint64_t octeontx_dram_reserve(uint64_t size, ccs_region_index_t index)
