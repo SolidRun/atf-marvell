3d1ebf8652eb2d6ce3be533e0f8a938fe2a5f39f
Aaron Williams <awilliams@marvell.com>
2020-12-11 21:32:25 -0800

    Add update flag to erase eMMC partition data
    Change-Id: Ibd266486b511361b24939ed0fa8c82c77d5745b6
    Signed-off-by: Aaron Williams <awilliams@marvell.com>
    Reviewed-on: https://sj1git1.cavium.com/c/IP/SW/boot/atf/+/42108
    Tested-by: sa_ip-sw-jenkins <sa_ip-sw-jenkins@marvell.com>
    Reviewed-by: Chandrakala Chavva <cchavva@marvell.com>

--- include/drivers/marvell/tim_update.h
+++ include/drivers/marvell/tim_update.h
@@ -24,6 +24,7 @@
 
 #define UPDATE_FLAG_BACKUP	0x0001	/** Set to update secondary location */
 #define UPDATE_FLAG_EMMC	0x0002	/** Set to update eMMC instead of SPI */
+#define UPDATE_FLAG_ERASE_PART	0x0004	/** Erase eMMC partition data */
 /** Set when user parameters are passed */
 #define UPDATE_FLAG_USER_PARMS	0x8000
 
