3e92717c0feb2ba547e41e253c5d61ca4e61487f
Aaron Williams <awilliams@marvell.com>
2021-01-11 21:26:58 -0800

    Define erase buffer size
    Change-Id: I95972d7ae0ddaee534d88ce4634301652a7b8268
    Signed-off-by: Aaron Williams <awilliams@marvell.com>
    Reviewed-on: https://sj1git1.cavium.com/c/IP/SW/boot/atf/+/43422
    Tested-by: Chandrakala Chavva <cchavva@marvell.com>
    Reviewed-by: Chandrakala Chavva <cchavva@marvell.com>

--- include/drivers/marvell/spi.h
+++ include/drivers/marvell/spi.h
@@ -49,6 +49,7 @@
 
 #define SPI_PAGE_SIZE			256
 
+#define SPI_NOR_ERASE_SIZE		4096
 #define SPI_NOR_PROGRAM_TIMEOUT		1000		/* 1 sec */
 #define SPI_NOR_ERASE_TIMEOUT		2000		/* 2 sec */
 #define CONFIG_SPI_FREQUENCY		16000000
