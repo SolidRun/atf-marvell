c6d91bc85ba753a6490a6f2170e5efd1239b14f0
Chandrakala Chavva <Chandrakala.Chavva@cavium.com>
2021-01-18 16:04:02 -0800

    plat: marvell: octeontx: Add delay while shutting down the system
    Integration reference:
       https://sj1git1.cavium.com/c/IP/SW/boot/atf/+/43876
    
    Signed-off-by: Chandrakala Chavva <Chandrakala.Chavva@cavium.com>
    Change-Id: I44cecdc350c8dc028eda3cbd00c2072eb4a19c38
    Reviewed-on: https://sj1git1.cavium.com/c/IP/SW/boot/atf/+/43876
    Tested-by: Chandrakala Chavva <cchavva@marvell.com>
    Reviewed-by: Chandrakala Chavva <cchavva@marvell.com>
    Signed-off-by: Chandrakala Chavva <Chandrakala.Chavva@cavium.com>

--- plat/marvell/octeontx/octeontx_legacy_pm.c
+++ plat/marvell/octeontx/octeontx_legacy_pm.c
@@ -23,6 +23,7 @@
 #include <twsi.h>
 #include <octeontx_dram.h>
 #include <plat_octeontx.h>
+#include <drivers/delay_timer.h>
 #include <octeontx_legacy_pm.h>
 
 #include "cavm-csrs-rst.h"
@@ -248,6 +249,9 @@ static void __dead2 octeontx_legacy_system_reset(void)
 	union cavm_rst_soft_rst rst_soft_rst;
 	union cavm_rst_ocx rst_ocx;
 #endif
+
+	udelay(10);
+
 	dcsw_op_all(DCCISW);
 #if !(defined(PLAT_CN10K_FAMILY))
 	l2c_flush();
