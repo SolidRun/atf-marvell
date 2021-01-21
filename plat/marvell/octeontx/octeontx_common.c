2370b0b54638f4e9b0923c1d545a562983111edb
Chandrakala Chavva <Chandrakala.Chavva@cavium.com>
2021-01-21 14:04:31 -0800

    plat: marvell: Don't enable debugging information while updating firmware
    Use ebf setup menu to enable/disable firmware update debugging information
    
    Signed-off-by: Chandrakala Chavva <Chandrakala.Chavva@cavium.com>
    Change-Id: I76d48c9990543557b4f8693dbee15765bdf33df1
    Reviewed-on: https://sj1git1.cavium.com/c/IP/SW/boot/atf/+/44118
    Reviewed-by: Chandrakala Chavva <cchavva@marvell.com>
    Tested-by: Chandrakala Chavva <cchavva@marvell.com>

--- plat/marvell/octeontx/octeontx_common.c
+++ plat/marvell/octeontx/octeontx_common.c
@@ -275,8 +275,7 @@ void initialize_tf_logging(void)
 			MRVL_TF_LOG_MODULE_LIB |
 			MRVL_TF_LOG_MODULE_PLAT |
 			MRVL_TF_LOG_MODULE_SERVICES |
-			MRVL_TF_LOG_MODULE_LIBTIM |
-			MRVL_TF_LOG_MODULE_UPDATE;
+			MRVL_TF_LOG_MODULE_LIBTIM;
 
 	propstr = fdt_getprop(fdt, fdt_off,
 			      "EBF-CONFIG-ATF-TRACE-MODULES", &len);
