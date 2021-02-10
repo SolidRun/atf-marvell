5f339de57a3b0a02142d207ab4bfe116b5c40fcc
Chandrakala Chavva <Chandrakala.Chavva@cavium.com>
2021-02-10 14:13:18 -0800

    plat: marvell: cn10k: Fix powering cores for emulator platform
    Signed-off-by: Chandrakala Chavva <Chandrakala.Chavva@cavium.com>
    Change-Id: I7e229e83e6ce32ea9e1bf5ad331a7a901a43b766
    Reviewed-on: https://sj1git1.cavium.com/c/IP/SW/boot/atf/+/45812
    Tested-by: Chandrakala Chavva <cchavva@marvell.com>
    Reviewed-by: Chandrakala Chavva <cchavva@marvell.com>

--- plat/marvell/octeontx/octeontx_legacy_pwrc.c
+++ plat/marvell/octeontx/octeontx_legacy_pwrc.c
@@ -104,6 +104,9 @@ void octeontx_legacy_pwrc_write_pponr(unsigned long mpidr)
 	cavm_dsuubx_cluster_ppu_pwsr_t cluster_pwsr;
 	cavm_dsuubx_core_ppu_pwsr_t core_pwsr;
 
+	if (cavm_is_platform(PLATFORM_EMULATOR))
+		printf("Turning core%ld on\n", octeontx_core_id);
+
 	/* Set RVBARADDR with entry point */
 	plat_cn10k_set_secondary_cpu_jump_addr(octeontx_core_id,
 			(uint64_t)plat_secondary_cold_boot_setup);
@@ -113,19 +116,21 @@ void octeontx_legacy_pwrc_write_pponr(unsigned long mpidr)
 	cluster_pwpr.s.op_policy = 0x7; /* OPMODE_07: ALL_SLICE_FULL_RAM_ON */
 	CSR_WRITE(CAVM_DSUUBX_CLUSTER_PPU_PWPR(octeontx_core_id), cluster_pwpr.u);
 
-	/* Poll on core PPU_PWSR register until the value matches the PWPR */
-	loop = 1000000;
-	while (loop) {
-		cluster_pwpr.u = CSR_READ(CAVM_DSUUBX_CLUSTER_PPU_PWPR(octeontx_core_id));
-		cluster_pwsr.u = CSR_READ(CAVM_DSUUBX_CLUSTER_PPU_PWSR(octeontx_core_id));
-		if (cluster_pwsr.u == cluster_pwpr.u)
-			break;
-		udelay(1);
-		loop--;
-	}
-	if (!loop) {
-		WARN("%s: Failed to match PWSR with PWPR cluster_pwsr.u 0x%x\n", __func__, cluster_pwsr.u);
-		return;
+	if (!cavm_is_platform(PLATFORM_EMULATOR)) {
+		/* Poll on core PPU_PWSR register until the value matches the PWPR */
+		loop = 1000000;
+		while (loop) {
+			cluster_pwpr.u = CSR_READ(CAVM_DSUUBX_CLUSTER_PPU_PWPR(octeontx_core_id));
+			cluster_pwsr.u = CSR_READ(CAVM_DSUUBX_CLUSTER_PPU_PWSR(octeontx_core_id));
+			if (cluster_pwsr.u == cluster_pwpr.u)
+				break;
+			udelay(1);
+			loop--;
+		}
+		if (!loop) {
+			WARN("%s: Failed to match PWSR with PWPR cluster_pwsr.u 0x%x\n", __func__, cluster_pwsr.u);
+			return;
+		}
 	}
 
 	/* Power up the core */
@@ -133,19 +138,21 @@ void octeontx_legacy_pwrc_write_pponr(unsigned long mpidr)
 	core_pwpr.s.pwr_policy = 0x8; /* ON. Logic on with RAM on, cluster is functional */
 	CSR_WRITE(CAVM_DSUUBX_CORE_PPU_PWPR(octeontx_core_id), core_pwpr.u);
 
-	/* Poll on core PPU_PWSR register until the value matches the PWPR */
-	loop = 1000000;
-	while(loop) {
-		core_pwsr.u = CSR_READ(CAVM_DSUUBX_CORE_PPU_PWSR(octeontx_core_id));
-		core_pwpr.u = CSR_READ(CAVM_DSUUBX_CORE_PPU_PWPR(octeontx_core_id));
-		if (core_pwsr.u == core_pwpr.u)
-			break;
-		udelay(1);
-		loop--;
-	}
-	if (!loop) {
-		WARN("%s: Failed to match PWSR with PWPR core_pwsr.u 0x%x\n", __func__, core_pwsr.u);
-		return;
+	if (!cavm_is_platform(PLATFORM_EMULATOR)) {
+		/* Poll on core PPU_PWSR register until the value matches the PWPR */
+		loop = 1000000;
+		while(loop) {
+			core_pwsr.u = CSR_READ(CAVM_DSUUBX_CORE_PPU_PWSR(octeontx_core_id));
+			core_pwpr.u = CSR_READ(CAVM_DSUUBX_CORE_PPU_PWPR(octeontx_core_id));
+			if (core_pwsr.u == core_pwpr.u)
+				break;
+			udelay(1);
+			loop--;
+		}
+		if (!loop) {
+			WARN("%s: Failed to match PWSR with PWPR core_pwsr.u 0x%x\n", __func__, core_pwsr.u);
+			return;
+		}
 	}
 #else
 	union cavm_rst_pp_reset pp_reset;
@@ -190,6 +197,9 @@ void octeontx_legacy_pwrc_cpu_off(int octeontx_core_id)
 	cavm_dsuubx_cluster_ppu_pwsr_t cluster_pwsr;
 	cavm_dsuubx_core_ppu_pwsr_t core_pwsr;
 
+	if (cavm_is_platform(PLATFORM_EMULATOR))
+		printf("Turning core%d off\n", octeontx_core_id);
+
 	cpupwrctlr_el1 = read_cpupwrctlr_el1();
 	set_bit(cpupwrctlr_el1, 0);
 	write_cpupwrctlr_el1(cpupwrctlr_el1);
@@ -199,19 +209,21 @@ void octeontx_legacy_pwrc_cpu_off(int octeontx_core_id)
 	cluster_pwpr.s.pwr_policy = 0x0; /* OFF */
 	CSR_WRITE(CAVM_DSUUBX_CLUSTER_PPU_PWPR(octeontx_core_id), cluster_pwpr.u);
 
-	loop = 1000000;
-	while (loop) {
-		cluster_pwpr.u = CSR_READ(CAVM_DSUUBX_CLUSTER_PPU_PWPR(octeontx_core_id));
-		cluster_pwsr.u = CSR_READ(CAVM_DSUUBX_CLUSTER_PPU_PWSR(octeontx_core_id));
+	if (!cavm_is_platform(PLATFORM_EMULATOR)) {
+		loop = 1000000;
+		while (loop) {
+			cluster_pwpr.u = CSR_READ(CAVM_DSUUBX_CLUSTER_PPU_PWPR(octeontx_core_id));
+			cluster_pwsr.u = CSR_READ(CAVM_DSUUBX_CLUSTER_PPU_PWSR(octeontx_core_id));
 
-		if (cluster_pwsr.u == cluster_pwpr.u)
-			break;
-		udelay(1);
-		loop--;
-	}
-	if (!loop) {
-		WARN("%s: Failed to match PWSR with PWPR cluster_pwsr.u 0x%x\n", __func__, cluster_pwsr.u);
-		return;
+			if (cluster_pwsr.u == cluster_pwpr.u)
+				break;
+			udelay(1);
+			loop--;
+		}
+		if (!loop) {
+			WARN("%s: Failed to match PWSR with PWPR cluster_pwsr.u 0x%x\n", __func__, cluster_pwsr.u);
+			return;
+		}
 	}
 
 	/* Set the policy mode to OFF for the core/cluster */
@@ -219,18 +231,20 @@ void octeontx_legacy_pwrc_cpu_off(int octeontx_core_id)
 	core_pwpr.s.pwr_policy = 0x0; /* OFF */
 	CSR_WRITE(CAVM_DSUUBX_CORE_PPU_PWPR(octeontx_core_id), core_pwpr.u);
 
-	loop = 1000000;
-	while (loop) {
-		core_pwsr.u = CSR_READ(CAVM_DSUUBX_CORE_PPU_PWSR(octeontx_core_id));
-		core_pwpr.u = CSR_READ(CAVM_DSUUBX_CORE_PPU_PWPR(octeontx_core_id));
-		if (core_pwsr.u == core_pwpr.u)
-			break;
-		udelay(1);
-		loop--;
-	}
-	if (!loop) {
-		WARN("%s: Failed to match PWSR with PWPR core_pwsr.u 0x%x\n", __func__, core_pwsr.u);
-		return;
+	if (!cavm_is_platform(PLATFORM_EMULATOR)) {
+		loop = 1000000;
+		while (loop) {
+			core_pwsr.u = CSR_READ(CAVM_DSUUBX_CORE_PPU_PWSR(octeontx_core_id));
+			core_pwpr.u = CSR_READ(CAVM_DSUUBX_CORE_PPU_PWPR(octeontx_core_id));
+			if (core_pwsr.u == core_pwpr.u)
+				break;
+			udelay(1);
+			loop--;
+		}
+		if (!loop) {
+			WARN("%s: Failed to match PWSR with PWPR core_pwsr.u 0x%x\n", __func__, core_pwsr.u);
+			return;
+		}
 	}
 #endif
 }
