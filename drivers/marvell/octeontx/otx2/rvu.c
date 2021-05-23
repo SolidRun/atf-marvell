30d1c0fc2048937835bf9041f28a2b81d8fdc96b
Chandrakala Chavva <Chandrakala.Chavva@cavium.com>
2021-05-23 16:23:50 -0700

    driver: marvell: rvu: Adjust RVU PF for SDP on T98 Pass1.0
    On T98 Pass1.0, use RVU PF13 and RVU PF14 for SDP interface
    if T98 is in EP mode instead of PF17 and PF18.
    
    Integration reference:
      https://sj1git1.cavium.com/c/IP/SW/boot/atf/+/52435
    
    Signed-off-by: Chandrakala Chavva <Chandrakala.Chavva@cavium.com>
    Change-Id: I67a928059b90035b883ff5e511e11fadfe603eb8
    Reviewed-on: https://sj1git1.cavium.com/c/IP/SW/boot/atf/+/52435
    Tested-by: Chandrakala Chavva <cchavva@marvell.com>
    Reviewed-by: Chandrakala Chavva <cchavva@marvell.com>

--- drivers/marvell/octeontx/otx2/rvu.c
+++ drivers/marvell/octeontx/otx2/rvu.c
@@ -362,7 +362,10 @@ static void rvu_provision_pfs_for_sw_devs(int top_eth_pf,
 
 	/* Provision RVU PFs for SDP. */
 	for (i = SW_RVU_SDP_NUM_PF - 1; (int)i >= 0; i--) {
-		rvu_pf = rvu_first_available(avail_from_top);
+		if (IS_OCTEONTX_PASS(read_midr(), T98PARTNUM, 1, 0))
+			rvu_pf = i ? 14 : 13;
+		else
+			rvu_pf = rvu_first_available(avail_from_top);
 
 		/* Enforce constraint */
 		if (rvu_pf < RVU_ETH_FIRST) {
