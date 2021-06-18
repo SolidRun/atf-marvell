a805f546a72b12081ae9c4b6a31c2d5c313c7e6f
Felix Manlunas <fmanlunas@marvell.com>
2021-06-17 22:17:30 -0700

    drivers: marvell: otx2: phy: Fix bug in changing modulation type to PAM4
    Changing modulation type to PAM4 (from NRZ) fails under this is condition:
    FEC type was previously set to RS.
    
    Fix it in phy_set_mod_type() by changing "lmac->fec" to "lmac->line_fec"
    in the if-statement that checks if FEC type is not RS.
    
    Note that:
        * PAM4 requires RS-FEC.
        * PAM4 and RS-FEC are at the 6141 PHY line side.
        * 6141 PHY host side remains at NRZ with no FEC.
    
    Signed-off-by: Felix Manlunas <fmanlunas@marvell.com>
    Change-Id: If1c50fc3374b44ffde1598e0857655d15d1f7266
    Reviewed-on: https://sj1git1.cavium.com/c/IP/SW/boot/atf/+/54327
    Tested-by: Chandrakala Chavva <cchavva@marvell.com>
    Reviewed-by: Chandrakala Chavva <cchavva@marvell.com>

--- drivers/marvell/octeontx/otx2/phy/phy_mgmt.c
+++ drivers/marvell/octeontx/otx2/phy/phy_mgmt.c
@@ -317,7 +317,7 @@ int phy_set_mod_type(int cgx_id, int lmac_id, phy_mod_type_t mod_type)
 		case QLM_MODE_50GAUI_2_C2C:
 		case QLM_MODE_50GAUI_4_C2C:
 			phy->mod_type = PHY_MOD_TYPE_PAM4;
-			if (lmac->fec != CGX_FEC_RS) {
+			if (lmac->line_fec != CGX_FEC_RS) {
 				WARN("%s: %d:%d Setting FEC to RS because PAM4 requires it.\n",
 				     __func__, cgx_id, lmac_id);
 				cgx_set_fec_type(cgx_id, lmac_id, CGX_FEC_RS);
