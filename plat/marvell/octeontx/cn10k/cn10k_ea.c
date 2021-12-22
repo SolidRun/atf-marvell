8ac85a9880a075d68d5fdf03aedd8d159e6df05d
Jayanthi Annadurai <jannadurai@marvell.com>
2021-12-21 18:25:10 -0800

    plat: cn10k: Fix platform EA handler
    Change-Id: Idd93753c3798bb57fa82634db34f809e20690c60
    Signed-off-by: Jayanthi Annadurai <jannadurai@marvell.com>
    Reviewed-on: https://sj1git1.cavium.com/c/IP/SW/boot/atf/+/68253
    Tested-by: sa_ip-sw-jenkins <sa_ip-sw-jenkins@marvell.com>
    Reviewed-by: Chandrakala Chavva <cchavva@marvell.com>

--- plat/marvell/octeontx/cn10k/cn10k_ea.c
+++ plat/marvell/octeontx/cn10k/cn10k_ea.c
@@ -3,11 +3,24 @@
 #include <common/debug.h>
 #include <arch_helpers.h>
 
+#define ID_AA64MMFR2_EL1_IESB_SHIFT      U(12)
+#define ID_AA64MMFR2_EL1_IESB_MASK       ULL(0xF)
+
+#define ERROR_SYNDROME_IESB_SHIFT	13
+#define ERROR_SYNDROME_IESB_MASK	0x1
+
 #define CN10K_SERROR_SYNDROME		0xbe000011
 
 void plat_ea_handler(unsigned int ea_reason, uint64_t syndrome, void *cookie,
 		void *handle, uint64_t flags)
 {
+	if ((read_id_aa64mmfr2_el1() >> ID_AA64MMFR2_EL1_IESB_SHIFT) &
+		ID_AA64MMFR2_EL1_IESB_MASK) {
+		if ((syndrome >> ERROR_SYNDROME_IESB_SHIFT) &
+			ERROR_SYNDROME_IESB_MASK)
+			return;
+	}
+
 	if (syndrome != CN10K_SERROR_SYNDROME) {
 		ERROR("Unhandled External Abort received on 0x%lx at EL3!\n",
 			read_mpidr_el1());
