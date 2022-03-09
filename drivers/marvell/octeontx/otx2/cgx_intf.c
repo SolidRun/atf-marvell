418dd2bd8b41425182abaf05a7af1a95e18d73c9
Damian Eppel <deppel@marvell.com>
2022-03-09 01:35:12 -0800

    drivers: marvell: cgx_intf: fix GET_LINK_STS command
    Get link status command should still return success, even if
    there is an error indicated inside the returned link status.
    
    Signed-off-by: Damian Eppel <deppel@marvell.com>
    Change-Id: I40c26ee38c93314a12b629d6fbdc6d3bcc7e3ce3
    Reviewed-on: https://sj1git1.cavium.com/c/IP/SW/boot/atf/+/72644
    Tested-by: sa_ip-sw-jenkins <sa_ip-sw-jenkins@marvell.com>
    Reviewed-by: Chandrakala Chavva <cchavva@marvell.com>

--- drivers/marvell/octeontx/otx2/cgx_intf.c
+++ drivers/marvell/octeontx/otx2/cgx_intf.c
@@ -2264,7 +2264,7 @@ static int cgx_process_requests(int cgx_id, int lmac_id)
 	 */
 	scratchx0.u = CSR_READ(CAVM_CGXX_CMRX_SCRATCHX(cgx_id, lmac_id, 0));
 	err_type = cgx_get_error_type(cgx_id, lmac_id);
-	if (err_type & CGX_ERR_MASK)
+	if (err_type & CGX_ERR_MASK && (request_id != ETH_CMD_GET_LINK_STS))
 		scratchx0.s.evt_sts.stat = ETH_STAT_FAIL;
 	else
 		scratchx0.s.evt_sts.stat = ETH_STAT_SUCCESS;
