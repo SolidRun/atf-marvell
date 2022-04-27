c0d0eddd3589aa24a0dceeb3343a433c0d51e4dd
Jayanthi Annadurai <jannadurai@marvell.com>
2022-04-27 10:17:34 -0700

    plat: marvell: cn10k: Platform RAS error handling
    Check for ras errors when there is an abort received in
    EL3. If there is a ras error, handle that first before
    checking for the error syndrome.
    
    Change-Id: Id08b2f07f4eac1a8083fa41f66190892e5fd27f1
    Signed-off-by: Jayanthi Annadurai <jannadurai@marvell.com>
    Reviewed-on: https://sj1git1.cavium.com/c/IP/SW/boot/atf/+/75729
    Reviewed-by: Chandrakala Chavva <cchavva@marvell.com>
    Tested-by: Chandrakala Chavva <cchavva@marvell.com>

--- plat/marvell/octeontx/cn10k/cn10k_ea.c
+++ plat/marvell/octeontx/cn10k/cn10k_ea.c
@@ -1,7 +1,14 @@
+/*
+ * Copyright (C) 2021 Marvell.
+ *
+ * SPDX-License-Identifier:     BSD-3-Clause
+ * https://spdx.org/licenses
+ */
 
 #include <common/bl_common.h>
 #include <common/debug.h>
 #include <arch_helpers.h>
+#include <lib/extensions/ras.h>
 
 #define ID_AA64MMFR2_EL1_IESB_SHIFT      U(12)
 #define ID_AA64MMFR2_EL1_IESB_MASK       ULL(0xF)
@@ -14,6 +21,18 @@
 void plat_ea_handler(unsigned int ea_reason, uint64_t syndrome, void *cookie,
 		void *handle, uint64_t flags)
 {
+#if RAS_EXTENSION
+	/* Call RAS EA handler */
+	int handled = ras_ea_handler(ea_reason, syndrome, cookie, handle, flags);
+
+	/*
+	 * handled 0 means, there is no RAS error. 1 means ras error is handled
+	 * from the ras error handler.
+	 */
+	if (handled != 0)
+		return;
+#endif
+
 	if ((read_id_aa64mmfr2_el1() >> ID_AA64MMFR2_EL1_IESB_SHIFT) &
 		ID_AA64MMFR2_EL1_IESB_MASK) {
 		if ((syndrome >> ERROR_SYNDROME_IESB_SHIFT) &
