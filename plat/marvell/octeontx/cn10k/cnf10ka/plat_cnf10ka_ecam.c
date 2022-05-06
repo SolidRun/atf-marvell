65f8ae82cfbb7f613c1b9eed7abefe1510c08519
Radha Mohan Chintakuntla <radhac@marvell.com>
2022-05-06 09:33:47 -0700

    cn10k: add revision id for RVU SDP PF
    Add revision id for RVU SDP PF for all cn10k chips.
    
    Signed-off-by: Radha Mohan Chintakuntla <radhac@marvell.com>
    Change-Id: I3fc89bbac8e8632d3d03bb7ba0e396580496ff9e
    Reviewed-on: https://sj1git1.cavium.com/c/IP/SW/boot/atf/+/76487
    Reviewed-by: Satananda Burla <sburla@marvell.com>
    Reviewed-by: Jayanthi Annadurai <Jayanthi.Annadurai@cavium.com>
    Tested-by: Chandrakala Chavva <cchavva@marvell.com>

--- plat/marvell/octeontx/cn10k/cnf10ka/plat_cnf10ka_ecam.c
+++ plat/marvell/octeontx/cn10k/cnf10ka/plat_cnf10ka_ecam.c
@@ -341,6 +341,7 @@ struct ecam_init_callback plat_init_callbacks[] = {
 	{0xa060, 0x177d, init_rpm}, /* 0x60 - PCC_DEV_IDL_E::RPM */
 	{0xa065, 0x177d, init_rvu}, /* 0x65 - PCC_DEV_IDL_E::RVU_AF */
 	{0xa063, 0x177d, init_rvu_rid}, /* 0x63 - PCC_DEV_IDL_E::RVU */
+	{0xa0f6, 0x177d, init_rvu_rid}, /* 0xf6 - PCC_DEV_IDL_E::RVU_SDP_PF */
 	{0xa0f8, 0x177d, init_rvu_rid}, /* 0xf8 - PCC_DEV_IDL_E::RVU_AF_VF */
 	{0xa0fb, 0x177d, init_rvu_rid}, /* 0xfb - PCC_DEV_IDL_E::RVU_NPA_PF */
 	{0xa0fc, 0x177d, init_rvu_rid}, /* 0xfc - PCC_DEV_IDL_E::RVU_NPA_VF */
