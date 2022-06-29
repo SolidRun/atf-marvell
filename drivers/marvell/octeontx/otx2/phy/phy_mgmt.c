88faa63a088447b2c6190445eb99cf6a6ecf1be7
Narendra Hadke <nhadke@marvell.com>
2022-06-28 18:49:29 -0700

    driver: 7121 :fix compile issue
    - Fixed compile warning in phy_mgmt.h as it was
      causing issue in some builds.
    
    Signed-off-by: Narendra Hadke <nhadke@marvell.com>
    Change-Id: I02d7a69a75df4aa0e41da59e2f47df93fe26fc90
    Reviewed-on: https://sj1git1.cavium.com/c/IP/SW/boot/atf/+/80672
    Reviewed-by: Chandrakala Chavva <cchavva@marvell.com>
    Tested-by: Chandrakala Chavva <cchavva@marvell.com>

--- drivers/marvell/octeontx/otx2/phy/phy_mgmt.c
+++ drivers/marvell/octeontx/otx2/phy/phy_mgmt.c
@@ -372,9 +372,9 @@ int phy_advance_commads(int eth_id, int lmac_id, uintptr_t *adv_cmds, int size)
 	debug_nw_mgmt("%s: phy->valid %d %d:%d\n", __func__, phy->valid, eth_id, lmac_id);
 	/* Call PHY specific config callback here */
 	if (phy->valid && phy->drv->mac_adv_cmds) {
-		ret = phy->drv->mac_adv_cmds(eth_id, lmac_id, adv_cmds, size);
+		phy->drv->mac_adv_cmds(eth_id, lmac_id, adv_cmds, size);
 		debug_nw_mgmt("%s: called >mac_adv_cmds %d:%d adv_cmds %p"
-			" size %d ret %d\n", __func__, eth_id, lmac_id, adv_cmds, size, ret);
+			" size %d\n", __func__, eth_id, lmac_id, adv_cmds, size);
 	}
 
 	if (base_addr && ns_map_size)
