dd48c7d8b321af40c0001bff21b5e309cb0b0a3b
Jayanthi Annadurai <jannadurai@marvell.com>
2022-08-07 23:47:03 -0700

    plat: marvell: cn10k: Disable panic on EA from Lower EL
    Check for RAS error and report when there is an external
    abort from Lower EL. Disable panic on EA from lower EL as
    any user space application error is causing system reboot.
    
    Change-Id: I36be339e56348f23da5cc615652f0d5dfd8444f8
    Signed-off-by: Jayanthi Annadurai <jannadurai@marvell.com>
    Reviewed-on: https://sj1git1.cavium.com/c/IP/SW/boot/atf/+/83424
    Reviewed-by: Chandrakala Chavva <cchavva@marvell.com>
    Tested-by: Chandrakala Chavva <cchavva@marvell.com>

--- plat/marvell/octeontx/cn10k/cn10k_ea.c
+++ plat/marvell/octeontx/cn10k/cn10k_ea.c
@@ -30,9 +30,10 @@ void plat_ea_handler(unsigned int ea_reason, uint64_t syndrome, void *cookie,
 	 * from the ras error handler.
 	 */
 	if (handled != 0)
-		return;
+		VERBOSE("RAS errors notified from EA handler on 0x%lx\n", read_mpidr_el1());
 #endif
 
+#ifdef EL3_PANIC_ON_LOWER_EL_EA
 	if ((read_id_aa64mmfr2_el1() >> ID_AA64MMFR2_EL1_IESB_SHIFT) &
 		ID_AA64MMFR2_EL1_IESB_MASK) {
 		if ((syndrome >> ERROR_SYNDROME_IESB_SHIFT) &
@@ -47,4 +48,5 @@ void plat_ea_handler(unsigned int ea_reason, uint64_t syndrome, void *cookie,
 				syndrome);
 		panic();
 	}
+#endif
 }
