ec4bbde61c083f104ba18ca630e0aeb54646da87
Damian Eppel <deppel@marvell.com>
2022-10-25 02:48:42 -0700

    otx2: sfp_mgmt: added support for QSFP-DD module
    Added support for QSFP-DD module.
    
    Signed-off-by: Damian Eppel <deppel@marvell.com>
    Change-Id: Iea86ccdcd1a7a223c07bd57eb2b784b4d211fc82
    Reviewed-on: https://sj1git1.cavium.com/c/IP/SW/boot/atf/+/88744
    Tested-by: sa_ip-sw-jenkins <sa_ip-sw-jenkins@marvell.com>
    Reviewed-by: Chandrakala Chavva <cchavva@marvell.com>

--- drivers/marvell/octeontx/otx2/sfp_mgmt.c
+++ drivers/marvell/octeontx/otx2/sfp_mgmt.c
@@ -1039,6 +1039,12 @@ retry_read_eeprom:
 			case 0x12:
 				debug_sfp_mgmt("%s: CXP-28 inserted\n", __func__);
 				break;
+			case 0x18:
+				debug_sfp_mgmt("%s: QSFP-DD inserted\n", __func__);
+				qsfp_get_info(cgx_id, lmac_id);
+				sff_id = ETH_MODULE_SFF_8024;
+				ret = cap_info->trans_type;
+				break;
 			default:
 				ERROR("%s: %d:%d unknown transceiver type inserted\n", __func__,
 									cgx_id, lmac_id);
