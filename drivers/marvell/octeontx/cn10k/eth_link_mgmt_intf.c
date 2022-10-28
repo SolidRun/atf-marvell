07532005b5e0a5fd004b52727ffa4dbdd4c0236b
Damian Eppel <deppel@marvell.com>
2022-10-28 03:02:16 -0700

    cn10k: eth_link_mgmt: added ECP SM states for CDR lock
    Added ECP State Machine states related to CDR lock
    detection in link management.
    
    Signed-off-by: Damian Eppel <deppel@marvell.com>
    Change-Id: I29692385def7e5617b1fd6f4931eec85b1efbe1b
    Reviewed-on: https://sj1git1.cavium.com/c/IP/SW/boot/atf/+/89026
    Reviewed-by: Anthony Chan <achan@marvell.com>
    Tested-by: Chandrakala Chavva <cchavva@marvell.com>

--- drivers/marvell/octeontx/cn10k/eth_link_mgmt_intf.c
+++ drivers/marvell/octeontx/cn10k/eth_link_mgmt_intf.c
@@ -888,6 +888,12 @@ const char *cn10k_eth_link_state_to_str(ecp_link_state_enum_t link_state)
 	break;
 	ETH_LINK_STATE_CASE(ETH_LINK_STATE_RX_TRAIN_WAIT);
 	break;
+	ETH_LINK_STATE_CASE(ETH_LINK_STATE_CDR_LOCK_UPDATE_FIRST);
+	break;
+	ETH_LINK_STATE_CASE(ETH_LINK_STATE_CDR_LOCK_UPDATE_SECOND);
+	break;
+	ETH_LINK_STATE_CASE(ETH_LINK_STATE_CDR_LOCK_UPDATE_IN_PROGRESS);
+	break;
 
 	default:
 		WARN("%s: unrecognized ECP link state %d\n", __func__, link_state);
