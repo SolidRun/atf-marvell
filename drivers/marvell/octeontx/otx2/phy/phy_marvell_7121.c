a61deaceee4ad798ce8a0d32727cf7a22e6a2829
Yi Guo <yig@marvell.com>
2023-02-02 10:48:50 -0800

    phy: phy7121: add more specific mux setting for A-RINLINE1
    set serdes mux according to board name for A-RINLINE1
    
    Signed-off-by: Yi Guo <yig@marvell.com>
    Change-Id: If39a3c3f79402532f12008a2cf8d0cc4794937c6
    Reviewed-on: https://sj1git1.cavium.com/c/IP/SW/boot/atf/+/95972
    Reviewed-by: Chandrakala Chavva <cchavva@marvell.com>
    Tested-by: Chandrakala Chavva <cchavva@marvell.com>

--- drivers/marvell/octeontx/otx2/phy/phy_marvell_7121.c
+++ drivers/marvell/octeontx/otx2/phy/phy_marvell_7121.c
@@ -174,6 +174,34 @@ static MZD_STATUS set_serdes_mux_cn98xx_pcie_crb(IN MZD_DEV_PTR pDev)
 	return MZD_OK;
 }
 
+static MZD_STATUS set_serdes_mux_a_rinline1(IN MZD_DEV_PTR pDev)
+{
+	MZD_STATUS status;
+	MZD_U8 serdesMux[MZD_MAX_PORTS*MZD_NUM_LANES] =	{
+		0x0, 0x1, 0x6, 0x7, 0x2, 0x3, 0x8, 0x9,
+		0x4, 0x5, 0xa, 0xb, 0xc, 0xd, 0xe, 0xf
+	};
+
+	status =  mzdSetSerdesMux(pDev, MZD_LINE_SIDE, serdesMux);
+
+	if (status != MZD_OK) {
+		MZD_DBG_ERROR("mzdSampleSerdesMux: mzdSetSerdesMux call failed\n");
+		return MZD_FAIL;
+	}
+
+	status =  mzdSetSerdesMux(pDev, MZD_HOST_SIDE, serdesMux);
+
+	if (status != MZD_OK) {
+		MZD_DBG_ERROR("mzdSampleSerdesMux: mzdSetSerdesMux call failed\n");
+		return MZD_FAIL;
+	}
+
+	MZD_ATTEMPT(mzd_wait(pDev, 500));
+
+	return MZD_OK;
+}
+
+
 static MZD_STATUS phy_marvell_7121_led_ctrl(int cgx_id,
 					int lmac_id,
 					int led_pin_id)
@@ -349,6 +377,9 @@ void phy_marvell_7121_probe(int cgx_id, int lmac_id)
 
 	if (!strncmp(plat_octeontx_bcfg->bcfg.board_model, "cn98xx-pcie-crb", 15))
 		set_serdes_mux_cn98xx_pcie_crb(phy->priv);
+
+	if (!strncmp(plat_octeontx_bcfg->bcfg.board_model, "a-rinline1", 10))
+		set_serdes_mux_a_rinline1(phy->priv);
 
 #ifdef ATF_ENABLE_MAC_ADV_CMDS
 	phy_macsec_drv = &phy_7121_macsec_drv[cgx_id][lmac_id];
