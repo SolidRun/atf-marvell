56372fe3115b8880c9871022ded2eb3ab8d651f6
Suneel Garapati <sgarapati@marvell.com>
2023-08-15 13:28:05 -0700

    plat: cn10k: Remove SPI mode fallback
    Do not check using different SPI mode configuration.
    
    Change-Id: Ica2e6a10e23dfe848f926e92a93160abeee9da3b
    Signed-off-by: Suneel Garapati <sgarapati@marvell.com>
    Reviewed-on: https://sj1git1.cavium.com/c/IP/SW/boot/atf/+/109834
    Reviewed-by: Chandrakala Chavva <cchavva@marvell.com>
    Tested-by: Chandrakala Chavva <cchavva@marvell.com>
    (cherry picked from commit 241a258fb647a135482b5a42456435093d860c01)
    Reviewed-on: https://sj1git1.cavium.com/c/IP/SW/boot/atf/+/109933

--- plat/marvell/octeontx/cn10k/plat_io_storage.c
+++ plat/marvell/octeontx/cn10k/plat_io_storage.c
@@ -239,39 +239,8 @@ static int open_emmc(const uintptr_t spec)
 	return result;
 }
 
-static const char *spi_boot_method_to_string(int method)
-{
-	switch (method) {
-	case 0:
-		return "X4";
-	case SPI_FORCE_X1_READ:
-		return "X1";
-	case SPI_FORCE_LEGACY_MODE:
-		return "legacy";
-	default:
-		return "unknown";
-	}
-}
-
 int plat_try_next_boot_source(void)
 {
-	const char *method;
-
-	switch (plat_octeontx_bcfg->bcfg.boot_dev.boot_type) {
-	case OCTEONTX_BOOT_SPI:
-		method = spi_boot_method_to_string(
-			spi_boot_method[spi_boot_try]);
-		NOTICE("Could not load image using SPI in %s mode\n",
-				method);
-		spi_boot_try++;
-		if (spi_boot_try < ARRAY_SIZE(spi_boot_method)) {
-			method = spi_boot_method_to_string(
-					spi_boot_method[spi_boot_try]);
-			NOTICE("Try SPI %s mode\n", method);
-			return 1;
-		}
-	}
-
 	return 0;
 }
 
