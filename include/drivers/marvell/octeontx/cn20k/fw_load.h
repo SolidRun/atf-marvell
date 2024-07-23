4271ac07717949316546321a4c272d6902add0a7
Chandrakala Chavva <cchavva@cavium.com>
2024-07-23 09:04:06 -0700

    cn20k: Add a new header fw_load.h
    This file has definitions for loading firmware image
    
    Signed-off-by: Chandrakala Chavva <cchavva@cavium.com>
    Change-Id: I2326f283236b358b24597a2fc8d98f3fd5cbbcfb
    Reviewed-on: https://sj1git1.cavium.com/c/IP/SW/boot/atf/+/131885
    Reviewed-by: Chandrakala Chavva <cchavva@marvell.com>
    Tested-by: Chandrakala Chavva <cchavva@marvell.com>

--- include/drivers/marvell/octeontx/cn20k/fw_load.h
+++ include/drivers/marvell/octeontx/cn20k/fw_load.h
@@ -1,1 +1,17 @@
-../cn10k/fw_load.h
+/*
+ * Copyright (c) 2024 Marvell.
+ *
+ * SPDX-License-Identifier:     BSD-3-Clause
+ * https://spdx.org/licenses
+ */
+
+#ifndef __FW_LOAD_H__
+#define __FW_LOAD_H__
+
+/**
+ * Load an EFI image
+ */
+int load_efi_image(uintptr_t efi_img_buf, uint64_t *efi_image_size,
+		   uint64_t load_params, int image_id, bool nsec);
+
+#endif /* __FW_LOAD_H__ */
