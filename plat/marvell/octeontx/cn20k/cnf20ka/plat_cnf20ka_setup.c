f8414cbd9c10053208643a828c8b1ee6036e2a51
Nithya Mani <nmani@marvell.com>
2024-07-25 02:22:58 +0000

    plat: marvell: cnf20ka: Update memory mapping for AUX_GPIO CSRs
    This patch updates the memory mapping for AUX_GPIO CSRs
    
    Change-Id: I67c732d1dee0a47e6105d48f41be1910fc69f82b
    Signed-off-by: Nithya Mani <nmani@marvell.com>
    Reviewed-on: https://sj1git1.cavium.com/c/IP/SW/boot/atf/+/132006
    Tested-by: Chandrakala Chavva <cchavva@marvell.com>
    Reviewed-by: Chandrakala Chavva <cchavva@marvell.com>

--- plat/marvell/octeontx/cn20k/cnf20ka/plat_cnf20ka_setup.c
+++ plat/marvell/octeontx/cn20k/cnf20ka/plat_cnf20ka_setup.c
@@ -47,6 +47,7 @@
 #include "cavm-csrs-xcp.h"
 #include "cavm-csrs-cpc.h"
 #include "cavm-csrs-rpm.h"
+#include "cavm-csrs-aux_gpio.h"
 
 #define NCB_COUNT			5
 
@@ -245,6 +246,9 @@ void plat_add_mmio(void)
 
 	add_map_record(CAVM_GPIO_BAR_E_GPIO_PF_BAR0, CAVM_GPIO_BAR_E_GPIO_PF_BAR0_SIZE, attr);
 	add_map_record(CAVM_GPIO_BAR_E_GPIO_PF_BAR4, CAVM_GPIO_BAR_E_GPIO_PF_BAR4_SIZE, attr);
+
+	add_map_record(CAVM_AUX_GPIO_BAR_E_AUX_GPIO_PF_BAR0, CAVM_AUX_GPIO_BAR_E_AUX_GPIO_PF_BAR0_SIZE, attr);
+	add_map_record(CAVM_AUX_GPIO_BAR_E_AUX_GPIO_PF_BAR4, CAVM_AUX_GPIO_BAR_E_AUX_GPIO_PF_BAR4_SIZE, attr);
 
 	device_type_count = plat_octeontx_get_uaa_count();
 	for (i = 0; i < device_type_count; i++) {
