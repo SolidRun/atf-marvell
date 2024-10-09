17b8dbb48ce15b9f906dd40e9ea5d4a1814a0613
Amit Singh Tomar <amitsinght@marvell.com>
2024-10-09 17:34:48 +0530

    cn20k: Enable MPAM system registers access from lower ELs
    When ENABLE_MPAM_FOR_LOWER_ELS is set to 1, EL3 permits lower exception
    levels to access their own MPAM registers without trapping to EL3,
    enabling the Linux kernel to boot when MPAM is enabled.
    
    Signed-off-by: Amit Singh Tomar <amitsinght@marvell.com>
    Change-Id: Ie2bbafd044f1a55f0b4e292c6857937cd9e7a13f
    Reviewed-on: https://sj1git1.cavium.com/c/IP/SW/boot/atf/+/136914
    Tested-by: Chandrakala Chavva <cchavva@marvell.com>
    Reviewed-by: Chandrakala Chavva <cchavva@marvell.com>

--- plat/marvell/octeontx/cn20k/platform.mk
+++ plat/marvell/octeontx/cn20k/platform.mk
@@ -49,7 +49,7 @@ GIC_ENABLE_V4_EXTN      :=	1
 GICV3_SUPPORT_GIC600	:=	1
 
 # Enable MPAM for Linux
-#ENABLE_MPAM_FOR_LOWER_ELS	:= 1
+ENABLE_MPAM_FOR_LOWER_ELS	:= 1
 
 # override default platform CSR include path
 PLAT_CSR_INCLUDE        :=       include/plat/marvell/octeontx/cn20k/csr
