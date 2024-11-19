Gateworks/atf-newport

cavm: bfdt: Remove usage of bfdt instance
Hence we have reserved memory region for BFDT usage,
we can get rid of copying memory to bfdt structure instance
and use memory-mapped region for bfdt in all images.

Signed-off-by: Konrad Adamczyk <konrad.adamczyk@cavium.com>

plat/cavium/common/cavm_dt.c
@@ -24,29 +24,29 @@
 
 #include <libfdt.h>
 
-board_fdt_t bfdt;
+board_fdt_t *bfdt = (void *)BOARD_CFG_BASE;
 
 static void print_board_variables()
 {
 	INFO("======================\n");
-	INFO("BOARD MODEL = %s\n", bfdt.board_model);
+	INFO("BOARD MODEL = %s\n", bfdt->board_model);
 	INFO("BMC BOOT TWSI node=%d bus=0x%x, addr=0x%x\n",
-		bfdt.bmc_boot_twsi_node,
-		bfdt.bmc_boot_twsi_bus,
-		bfdt.bmc_boot_twsi_addr);
+		bfdt->bmc_boot_twsi_node,
+		bfdt->bmc_boot_twsi_bus,
+		bfdt->bmc_boot_twsi_addr);
 	INFO("BMC IPMI TWSI node=%d bus=0x%x, addr=0x%x\n",
-		bfdt.bmc_ipmi_twsi_node,
-		bfdt.bmc_ipmi_twsi_bus,
-		bfdt.bmc_ipmi_twsi_addr);
+		bfdt->bmc_ipmi_twsi_node,
+		bfdt->bmc_ipmi_twsi_bus,
+		bfdt->bmc_ipmi_twsi_addr);
 	INFO("GPIO Shutdown pin IN = 0x%x\n",
-		bfdt.gpio_shutdown_ctl_in);
+		bfdt->gpio_shutdown_ctl_in);
 	INFO("GPIO Shutdown pin OUT = 0x%x\n",
-		bfdt.gpio_shutdown_ctl_out);
+		bfdt->gpio_shutdown_ctl_out);
 #if TRUSTED_BOARD_BOOT
 	INFO("TRUST-ROT-ADDR = 0x%lx\n",
-		bfdt.trust_rot_addr);
+		bfdt->trust_rot_addr);
 	INFO("TRUST-BSSK-ADDR = 0x%lx\n",
-		bfdt.trust_key_addr);
+		bfdt->trust_key_addr);
 #endif
 	INFO("======================\n");
 
@@ -103,59 +103,59 @@ int cavm_fill_board_details(int info)
 	name = fdt_getprop(fdt, offset, "BOARD-MODEL", &len);
 	if (!name) {
 		printf("WARNING: No BOARD-MODEL is found\n");
-		bfdt.board_model[0] = '\0';
+		bfdt->board_model[0] = '\0';
 	} else {
-		strncpy(bfdt.board_model, name, sizeof(bfdt.board_model));
+		strncpy(bfdt->board_model, name, sizeof(bfdt->board_model));
 		/* Terminate, in case strlen(name) > sizeof(board_model) */
-		bfdt.board_model[sizeof(bfdt.board_model) - 1] = '\0';
-		len = strlen(bfdt.board_model);
+		bfdt->board_model[sizeof(bfdt->board_model) - 1] = '\0';
+		len = strlen(bfdt->board_model);
 		for (int i = 0; i < len; i++)
-			bfdt.board_model[i] = tolower(bfdt.board_model[i]);
+			bfdt->board_model[i] = tolower(bfdt->board_model[i]);
 	}
 
 	config = thunder_fdt_get(fdt, offset, "BMC-BOOT-TWSI-CONFIG", 0);
 	if (config != -1) {
-		bfdt.bmc_boot_twsi_node = (config >> 16) & 0xff;
-		bfdt.bmc_boot_twsi_bus = (config >> 8) & 0xff;
-		bfdt.bmc_boot_twsi_addr = config & 0xff;
+		bfdt->bmc_boot_twsi_node = (config >> 16) & 0xff;
+		bfdt->bmc_boot_twsi_bus = (config >> 8) & 0xff;
+		bfdt->bmc_boot_twsi_addr = config & 0xff;
 	} else {
-		bfdt.bmc_boot_twsi_node = 0;
-		bfdt.bmc_boot_twsi_bus = thunder_fdt_get(fdt, offset, "BMC-BOOT-TWSI-BUS", 10);
-		bfdt.bmc_boot_twsi_addr = thunder_fdt_get(fdt, offset, "BMC-BOOT-TWSI-ADDR", 16);
+		bfdt->bmc_boot_twsi_node = 0;
+		bfdt->bmc_boot_twsi_bus = thunder_fdt_get(fdt, offset, "BMC-BOOT-TWSI-BUS", 10);
+		bfdt->bmc_boot_twsi_addr = thunder_fdt_get(fdt, offset, "BMC-BOOT-TWSI-ADDR", 16);
 	}
 	config = thunder_fdt_get(fdt, offset, "BMC-IPMI-TWSI-CONFIG", 0);
 	if (config != -1) {
-		bfdt.bmc_ipmi_twsi_node = (config >> 16) & 0xff;
-		bfdt.bmc_ipmi_twsi_bus = (config >> 8) & 0xff;
-		bfdt.bmc_ipmi_twsi_addr = config & 0xff;
+		bfdt->bmc_ipmi_twsi_node = (config >> 16) & 0xff;
+		bfdt->bmc_ipmi_twsi_bus = (config >> 8) & 0xff;
+		bfdt->bmc_ipmi_twsi_addr = config & 0xff;
 	} else {
-		bfdt.bmc_ipmi_twsi_node = 0;
-		bfdt.bmc_ipmi_twsi_bus = thunder_fdt_get(fdt, offset, "BMC-IPMI-TWSI-BUS", 10);
-		bfdt.bmc_ipmi_twsi_addr = thunder_fdt_get(fdt, offset, "BMC-IPMI-TWSI-ADDR", 16);
+		bfdt->bmc_ipmi_twsi_node = 0;
+		bfdt->bmc_ipmi_twsi_bus = thunder_fdt_get(fdt, offset, "BMC-IPMI-TWSI-BUS", 10);
+		bfdt->bmc_ipmi_twsi_addr = thunder_fdt_get(fdt, offset, "BMC-IPMI-TWSI-ADDR", 16);
 	}
 
-	bfdt.gpio_shutdown_ctl_in = thunder_fdt_get(fdt, offset, "GPIO-SHUTDOWN-CTL-IN", 0);
+	bfdt->gpio_shutdown_ctl_in = thunder_fdt_get(fdt, offset, "GPIO-SHUTDOWN-CTL-IN", 0);
 	/* The new format is hex and allows for node id and polarity
 	 * packed into the value. We don't use them right now and mask
 	 * them out. */
-	if (bfdt.gpio_shutdown_ctl_in != -1)
-		bfdt.gpio_shutdown_ctl_in &= 0xff;
+	if (bfdt->gpio_shutdown_ctl_in != -1)
+		bfdt->gpio_shutdown_ctl_in &= 0xff;
 
-	bfdt.gpio_shutdown_ctl_out = thunder_fdt_get(fdt, offset, "GPIO-SHUTDOWN-CTL-OUT", 0);
+	bfdt->gpio_shutdown_ctl_out = thunder_fdt_get(fdt, offset, "GPIO-SHUTDOWN-CTL-OUT", 0);
 	/* The new format is hex and allows for node id and polarity
 	 * packed into the value. We don't use them right now and mask
 	 * them out. */
-	if (bfdt.gpio_shutdown_ctl_out != -1)
-		bfdt.gpio_shutdown_ctl_out &= 0xff;
+	if (bfdt->gpio_shutdown_ctl_out != -1)
+		bfdt->gpio_shutdown_ctl_out &= 0xff;
 
 	config = thunder_fdt_get(fdt, offset, "MCU-SHUTDOWN-TWSI-CONFIG", 16);
 	if (config != -1) {
-		bfdt.mcu_twsi.s.node = (config >> 24) & 0xff;
-		bfdt.mcu_twsi.s.int_addr = (config >> 16) & 0xff;
-		bfdt.mcu_twsi.s.bus = (config >> 8) & 0xff;
-		bfdt.mcu_twsi.s.addr = config & 0xff;
+		bfdt->mcu_twsi.s.node = (config >> 24) & 0xff;
+		bfdt->mcu_twsi.s.int_addr = (config >> 16) & 0xff;
+		bfdt->mcu_twsi.s.bus = (config >> 8) & 0xff;
+		bfdt->mcu_twsi.s.addr = config & 0xff;
 	} else {
-		bfdt.mcu_twsi.u = 0;
+		bfdt->mcu_twsi.u = 0;
 	}
 
 #if TRUSTED_BOARD_BOOT
@@ -164,13 +164,13 @@ int cavm_fill_board_details(int info)
 	 * For more information, please refer to bdk-trusted-boot.pdf file
 	 * in BDK source repository.
 	 */
-	bfdt.trust_rot_addr = thunder_fdt_get_uint64(fdt, offset, "TRUST-ROT-ADDR", 16);
+	bfdt->trust_rot_addr = thunder_fdt_get_uint64(fdt, offset, "TRUST-ROT-ADDR", 16);
 
 	/*
 	 * If either thunder_fdt_get_uint64 returned -1 (property not found) or
 	 * 0 (property found, but set to 0 indicates non-secure boot), stop secure booting
 	 */
-	if (bfdt.trust_rot_addr == 0 || bfdt.trust_rot_addr == -1) {
+	if (bfdt->trust_rot_addr == 0 || bfdt->trust_rot_addr == -1) {
 		printf("ERROR: No TRUST-ROT-ADDR in DTS, trusted boot failed\n");
 		return -1;
 	}
@@ -180,16 +180,16 @@ int cavm_fill_board_details(int info)
 	 * TRUST-BSSK-ADDR is set only when HUK parameter was passed to build system.
 	 * If so, it contains the address of BSSK key. Firstly, try to get BSSK
 	 */
-	bfdt.trust_key_addr = thunder_fdt_get_uint64(fdt, offset, "TRUST-BSSK-ADDR", 16);
+	bfdt->trust_key_addr = thunder_fdt_get_uint64(fdt, offset, "TRUST-BSSK-ADDR", 16);
 
 	/*
 	 * If either thunder_fdt_get_uint64 returned -1 (property not found) or
 	 * 0 (property found, but set to 0 indicates SSK), print info about it
 	 */
-	if (bfdt.trust_key_addr == 0 || bfdt.trust_rot_addr == -1) {
+	if (bfdt->trust_key_addr == 0 || bfdt->trust_rot_addr == -1) {
 		printf("INFO: No TRUST-BSSK-ADDR in DTS, images will be decrypted\n"
 		       "      with SSK key from FUSF_SSKX(0..1).\n");
-		bfdt.trust_key_addr = 0;
+		bfdt->trust_key_addr = 0;
 	}
 #endif /* CRYPTO_BOARD_BOOT */
 

SolidRun/atf-marvell

Change-Id: I840c4d7bf681f564e6377cf0b3d2d8d719ab0103

plat/cavium/common/cavm_dt.c
@@ -24,29 +24,29 @@
 
 #include <libfdt.h>
 
-board_fdt_t bfdt;
+board_fdt_t *bfdt = (void *)BOARD_CFG_BASE;
 
 static void print_board_variables()
 {
 	INFO("======================\n");
-	INFO("BOARD MODEL = %s\n", bfdt.board_model);
+	INFO("BOARD MODEL = %s\n", bfdt->board_model);
 	INFO("BMC BOOT TWSI node=%d bus=0x%x, addr=0x%x\n",
-		bfdt.bmc_boot_twsi_node,
-		bfdt.bmc_boot_twsi_bus,
-		bfdt.bmc_boot_twsi_addr);
+		bfdt->bmc_boot_twsi_node,
+		bfdt->bmc_boot_twsi_bus,
+		bfdt->bmc_boot_twsi_addr);
 	INFO("BMC IPMI TWSI node=%d bus=0x%x, addr=0x%x\n",
-		bfdt.bmc_ipmi_twsi_node,
-		bfdt.bmc_ipmi_twsi_bus,
-		bfdt.bmc_ipmi_twsi_addr);
+		bfdt->bmc_ipmi_twsi_node,
+		bfdt->bmc_ipmi_twsi_bus,
+		bfdt->bmc_ipmi_twsi_addr);
 	INFO("GPIO Shutdown pin IN = 0x%x\n",
-		bfdt.gpio_shutdown_ctl_in);
+		bfdt->gpio_shutdown_ctl_in);
 	INFO("GPIO Shutdown pin OUT = 0x%x\n",
-		bfdt.gpio_shutdown_ctl_out);
+		bfdt->gpio_shutdown_ctl_out);
 #if TRUSTED_BOARD_BOOT
 	INFO("TRUST-ROT-ADDR = 0x%lx\n",
-		bfdt.trust_rot_addr);
+		bfdt->trust_rot_addr);
 	INFO("TRUST-BSSK-ADDR = 0x%lx\n",
-		bfdt.trust_key_addr);
+		bfdt->trust_key_addr);
 #endif
 	INFO("======================\n");
 
@@ -103,59 +103,59 @@ int cavm_fill_board_details(int info)
 	name = fdt_getprop(fdt, offset, "BOARD-MODEL", &len);
 	if (!name) {
 		printf("WARNING: No BOARD-MODEL is found\n");
-		bfdt.board_model[0] = '\0';
+		bfdt->board_model[0] = '\0';
 	} else {
-		strlcpy(bfdt.board_model, name, sizeof(bfdt.board_model));
+		strlcpy(bfdt->board_model, name, sizeof(bfdt->board_model));
 		/* Terminate, in case strlen(name) > sizeof(board_model) */
-		bfdt.board_model[sizeof(bfdt.board_model) - 1] = '\0';
-		len = strlen(bfdt.board_model);
+		bfdt->board_model[sizeof(bfdt->board_model) - 1] = '\0';
+		len = strlen(bfdt->board_model);
 		for (int i = 0; i < len; i++)
-			bfdt.board_model[i] = tolower(bfdt.board_model[i]);
+			bfdt->board_model[i] = tolower(bfdt->board_model[i]);
 	}
 
 	config = thunder_fdt_get(fdt, offset, "BMC-BOOT-TWSI-CONFIG", 0);
 	if (config != -1) {
-		bfdt.bmc_boot_twsi_node = (config >> 16) & 0xff;
-		bfdt.bmc_boot_twsi_bus = (config >> 8) & 0xff;
-		bfdt.bmc_boot_twsi_addr = config & 0xff;
+		bfdt->bmc_boot_twsi_node = (config >> 16) & 0xff;
+		bfdt->bmc_boot_twsi_bus = (config >> 8) & 0xff;
+		bfdt->bmc_boot_twsi_addr = config & 0xff;
 	} else {
-		bfdt.bmc_boot_twsi_node = 0;
-		bfdt.bmc_boot_twsi_bus = thunder_fdt_get(fdt, offset, "BMC-BOOT-TWSI-BUS", 10);
-		bfdt.bmc_boot_twsi_addr = thunder_fdt_get(fdt, offset, "BMC-BOOT-TWSI-ADDR", 16);
+		bfdt->bmc_boot_twsi_node = 0;
+		bfdt->bmc_boot_twsi_bus = thunder_fdt_get(fdt, offset, "BMC-BOOT-TWSI-BUS", 10);
+		bfdt->bmc_boot_twsi_addr = thunder_fdt_get(fdt, offset, "BMC-BOOT-TWSI-ADDR", 16);
 	}
 	config = thunder_fdt_get(fdt, offset, "BMC-IPMI-TWSI-CONFIG", 0);
 	if (config != -1) {
-		bfdt.bmc_ipmi_twsi_node = (config >> 16) & 0xff;
-		bfdt.bmc_ipmi_twsi_bus = (config >> 8) & 0xff;
-		bfdt.bmc_ipmi_twsi_addr = config & 0xff;
+		bfdt->bmc_ipmi_twsi_node = (config >> 16) & 0xff;
+		bfdt->bmc_ipmi_twsi_bus = (config >> 8) & 0xff;
+		bfdt->bmc_ipmi_twsi_addr = config & 0xff;
 	} else {
-		bfdt.bmc_ipmi_twsi_node = 0;
-		bfdt.bmc_ipmi_twsi_bus = thunder_fdt_get(fdt, offset, "BMC-IPMI-TWSI-BUS", 10);
-		bfdt.bmc_ipmi_twsi_addr = thunder_fdt_get(fdt, offset, "BMC-IPMI-TWSI-ADDR", 16);
+		bfdt->bmc_ipmi_twsi_node = 0;
+		bfdt->bmc_ipmi_twsi_bus = thunder_fdt_get(fdt, offset, "BMC-IPMI-TWSI-BUS", 10);
+		bfdt->bmc_ipmi_twsi_addr = thunder_fdt_get(fdt, offset, "BMC-IPMI-TWSI-ADDR", 16);
 	}
 
-	bfdt.gpio_shutdown_ctl_in = thunder_fdt_get(fdt, offset, "GPIO-SHUTDOWN-CTL-IN", 0);
+	bfdt->gpio_shutdown_ctl_in = thunder_fdt_get(fdt, offset, "GPIO-SHUTDOWN-CTL-IN", 0);
 	/* The new format is hex and allows for node id and polarity
 	 * packed into the value. We don't use them right now and mask
 	 * them out. */
-	if (bfdt.gpio_shutdown_ctl_in != -1)
-		bfdt.gpio_shutdown_ctl_in &= 0xff;
+	if (bfdt->gpio_shutdown_ctl_in != -1)
+		bfdt->gpio_shutdown_ctl_in &= 0xff;
 
-	bfdt.gpio_shutdown_ctl_out = thunder_fdt_get(fdt, offset, "GPIO-SHUTDOWN-CTL-OUT", 0);
+	bfdt->gpio_shutdown_ctl_out = thunder_fdt_get(fdt, offset, "GPIO-SHUTDOWN-CTL-OUT", 0);
 	/* The new format is hex and allows for node id and polarity
 	 * packed into the value. We don't use them right now and mask
 	 * them out. */
-	if (bfdt.gpio_shutdown_ctl_out != -1)
-		bfdt.gpio_shutdown_ctl_out &= 0xff;
+	if (bfdt->gpio_shutdown_ctl_out != -1)
+		bfdt->gpio_shutdown_ctl_out &= 0xff;
 
 	config = thunder_fdt_get(fdt, offset, "MCU-SHUTDOWN-TWSI-CONFIG", 16);
 	if (config != -1) {
-		bfdt.mcu_twsi.s.node = (config >> 24) & 0xff;
-		bfdt.mcu_twsi.s.int_addr = (config >> 16) & 0xff;
-		bfdt.mcu_twsi.s.bus = (config >> 8) & 0xff;
-		bfdt.mcu_twsi.s.addr = config & 0xff;
+		bfdt->mcu_twsi.s.node = (config >> 24) & 0xff;
+		bfdt->mcu_twsi.s.int_addr = (config >> 16) & 0xff;
+		bfdt->mcu_twsi.s.bus = (config >> 8) & 0xff;
+		bfdt->mcu_twsi.s.addr = config & 0xff;
 	} else {
-		bfdt.mcu_twsi.u = 0;
+		bfdt->mcu_twsi.u = 0;
 	}
 
 #if TRUSTED_BOARD_BOOT
@@ -164,13 +164,13 @@ int cavm_fill_board_details(int info)
 	 * For more information, please refer to bdk-trusted-boot.pdf file
 	 * in BDK source repository.
 	 */
-	bfdt.trust_rot_addr = thunder_fdt_get_uint64(fdt, offset, "TRUST-ROT-ADDR", 16);
+	bfdt->trust_rot_addr = thunder_fdt_get_uint64(fdt, offset, "TRUST-ROT-ADDR", 16);
 
 	/*
 	 * If either thunder_fdt_get_uint64 returned -1 (property not found) or
 	 * 0 (property found, but set to 0 indicates non-secure boot), stop secure booting
 	 */
-	if (bfdt.trust_rot_addr == 0 || bfdt.trust_rot_addr == -1) {
+	if (bfdt->trust_rot_addr == 0 || bfdt->trust_rot_addr == -1) {
 		printf("ERROR: No TRUST-ROT-ADDR in DTS, trusted boot failed\n");
 		return -1;
 	}
@@ -180,16 +180,16 @@ int cavm_fill_board_details(int info)
 	 * TRUST-BSSK-ADDR is set only when HUK parameter was passed to build system.
 	 * If so, it contains the address of BSSK key. Firstly, try to get BSSK
 	 */
-	bfdt.trust_key_addr = thunder_fdt_get_uint64(fdt, offset, "TRUST-BSSK-ADDR", 16);
+	bfdt->trust_key_addr = thunder_fdt_get_uint64(fdt, offset, "TRUST-BSSK-ADDR", 16);
 
 	/*
 	 * If either thunder_fdt_get_uint64 returned -1 (property not found) or
 	 * 0 (property found, but set to 0 indicates SSK), print info about it
 	 */
-	if (bfdt.trust_key_addr == 0 || bfdt.trust_rot_addr == -1) {
+	if (bfdt->trust_key_addr == 0 || bfdt->trust_rot_addr == -1) {
 		printf("INFO: No TRUST-BSSK-ADDR in DTS, images will be decrypted\n"
 		       "      with SSK key from FUSF_SSKX(0..1).\n");
-		bfdt.trust_key_addr = 0;
+		bfdt->trust_key_addr = 0;
 	}
 #endif /* CRYPTO_BOARD_BOOT */
 