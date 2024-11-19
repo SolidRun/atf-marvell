Gateworks/atf-newport

tools: cert_create: Add support for OpenSSL engines
This patch add support for using OpenSSL engines in cert_create tool.
If flag OPENSSL_NO_ENGINE is disabled then new argument "--engine"
(with shourtcut "-e") is available. Argument is interpreted as OpenSSL
engine id. All keys areguments are not paths to pem file, but are
interpreted as key ids for ENGINE_load_private_key function.

Creating keys using OpenSSL is currently not supported, so all keys
used in CoT has to be passed.

Change-Id: I3731016419b4badc553f0875ba815c3bb2ad81a7
Signed-off-by: Tomasz Michalec <tomasz@marvell.com>
Reviewed-on: https://sj1git1.cavium.com/6007
Tested-by: sa_ip-sw-jenkins
Reviewed-by: Nithya Mani <Nithya.Mani@cavium.com>

tools/cert_create/src/main.c
@@ -33,6 +33,7 @@
 #include "tbbr/tbb_cert.h"
 #include "tbbr/tbb_ext.h"
 #include "tbbr/tbb_key.h"
+#include "engine.h"
 
 /*
  * Helper macros to simplify the code. This macro assigns the return value of
@@ -72,6 +73,7 @@ static int hash_alg;
 static int new_keys;
 static int save_keys;
 static int print_cert;
+static int use_engine;
 
 /* Info messages created in the Makefile */
 extern const char build_msg[];
@@ -183,6 +185,12 @@ static void check_cmd_params(void)
 		exit(1);
 	}
 
+	if (use_engine && new_keys) {
+		ERROR(
+			"Generating keys using OpenSSL engine is not implemented\n");
+		exit(1);
+	}
+
 	/* Check that all required options have been specified in the
 	 * command line */
 	for (i = 0; i < num_certs; i++) {
@@ -262,7 +270,13 @@ PKCS#1 v2.1, 'rsa_1_5' - RSA PKCS#1 v1.5, 'ecdsa'"
 	{
 		{ "print-cert", no_argument, NULL, 'p' },
 		"Print the certificates in the standard output"
-	}
+	},
+#ifndef OPENSSL_NO_ENGINE
+	{
+		{ "engine", required_argument, NULL, 'e' },
+		"Set OpenSSL engine that should be used to obtain keys"
+	},
+#endif
 };
 
 int main(int argc, char *argv[])
@@ -281,6 +295,10 @@ int main(int argc, char *argv[])
 	unsigned char md[SHA512_DIGEST_LENGTH];
 	unsigned int  md_len;
 	const EVP_MD *md_info;
+#ifndef OPENSSL_NO_ENGINE
+	char *engine_name = NULL;
+	ENGINE *e = NULL;
+#endif
 
 	NOTICE("CoT Generation Tool: %s\n", build_msg);
 	NOTICE("Target platform: %s\n", platform_msg);
@@ -317,7 +335,11 @@ int main(int argc, char *argv[])
 
 	while (1) {
 		/* getopt_long stores the option index here. */
+#ifdef OPENSSL_NO_ENGINE
 		c = getopt_long(argc, argv, "a:hknps:", cmd_opt, &opt_idx);
+#else
+		c = getopt_long(argc, argv, "a:e:hknps:", cmd_opt, &opt_idx);
+#endif
 
 		/* Detect the end of the options. */
 		if (c == -1) {
@@ -332,6 +354,12 @@ int main(int argc, char *argv[])
 				exit(1);
 			}
 			break;
+#ifndef OPENSSL_NO_ENGINE
+		case 'e':
+			engine_name = strdup(optarg);
+			use_engine = 1;
+			break;
+#endif
 		case 'h':
 			print_help(argv[0], cmd_opt);
 			break;
@@ -376,6 +404,23 @@ int main(int argc, char *argv[])
 	/* Check command line arguments */
 	check_cmd_params();
 
+
+#ifndef OPENSSL_NO_ENGINE
+	/* Load OpenSSL engine */
+	if (use_engine) {
+		e = init_engine(engine_name);
+		if (e == NULL) {
+			ERROR("Failed to init OpenSSL engine\n");
+			exit(1);
+		}
+		if (atexit(release_engine_atexit)) {
+			ERROR("Failed to register atexit callback\n");
+			release_engine(e);
+			exit(1);
+		}
+	}
+#endif
+
 	/* Indicate SHA as image hash algorithm in the certificate
 	 * extension */
 	if (hash_alg == HASH_ALG_SHA384) {
@@ -389,17 +434,28 @@ int main(int argc, char *argv[])
 		md_len  = SHA256_DIGEST_LENGTH;
 	}
 
-	/* Load private keys from files (or generate new ones) */
+	/* Load private keys (or generate new ones) */
 	for (i = 0 ; i < num_keys ; i++) {
 		if (!key_new(&keys[i])) {
 			ERROR("Failed to allocate key container\n");
 			exit(1);
 		}
 
-		/* First try to load the key from disk */
-		if (key_load(&keys[i], &err_code)) {
-			/* Key loaded successfully */
-			continue;
+#ifndef OPENSSL_NO_ENGINE
+		/* Try to load the key using OpenSSL engine */
+		if (use_engine) {
+			if (key_load_using_engine(&keys[i], &err_code, e)) {
+				/* Key loaded successfully */
+				continue;
+			}
+		}
+#endif
+		if (!use_engine) {
+			/* Try to load the key from disk */
+			if (key_load_from_file(&keys[i], &err_code)) {
+				/* Key loaded successfully */
+				continue;
+			}
 		}
 
 		/* Key not loaded. Check the error code */
@@ -538,6 +594,9 @@ int main(int argc, char *argv[])
 	}
 
 #ifndef OPENSSL_NO_ENGINE
+	if (e != NULL)
+		release_engine(e);
+
 	ENGINE_cleanup();
 #endif
 	CRYPTO_cleanup_all_ex_data();

SolidRun/atf-marvell

tools/cert_create/src/main.c
@@ -25,6 +25,10 @@
 #include "ext.h"
 #include "key.h"
 #include "sha.h"
+#include "tbbr/tbb_cert.h"
+#include "tbbr/tbb_ext.h"
+#include "tbbr/tbb_key.h"
+#include "engine.h"
 
 /*
  * Helper macros to simplify the code. This macro assigns the return value of
@@ -65,6 +69,7 @@ static int key_size;
 static int new_keys;
 static int save_keys;
 static int print_cert;
+static int use_engine;
 
 /* Info messages created in the Makefile */
 extern const char build_msg[];
@@ -207,6 +212,11 @@ static void check_cmd_params(void)
 		printf("\n");
 		exit(1);
 	}
+	if (use_engine && new_keys) {
+		ERROR(
+			"Generating keys using OpenSSL engine is not implemented\n");
+		exit(1);
+	}
 
 	/* Check that all required options have been specified in the
 	 * command line */
@@ -291,7 +301,13 @@ static const cmd_opt_t common_cmd_opt[] = {
 	{
 		{ "print-cert", no_argument, NULL, 'p' },
 		"Print the certificates in the standard output"
-	}
+	},
+#ifndef OPENSSL_NO_ENGINE
+	{
+		{ "engine", required_argument, NULL, 'e' },
+		"Set OpenSSL engine that should be used to obtain keys"
+	},
+#endif
 };
 
 int main(int argc, char *argv[])
@@ -310,6 +326,10 @@ int main(int argc, char *argv[])
 	unsigned char md[SHA512_DIGEST_LENGTH];
 	unsigned int  md_len;
 	const EVP_MD *md_info;
+#ifndef OPENSSL_NO_ENGINE
+	char *engine_name = NULL;
+	ENGINE *e = NULL;
+#endif
 
 	NOTICE("CoT Generation Tool: %s\n", build_msg);
 	NOTICE("Target platform: %s\n", platform_msg);
@@ -347,7 +367,11 @@ int main(int argc, char *argv[])
 
 	while (1) {
 		/* getopt_long stores the option index here. */
+#ifdef OPENSSL_NO_ENGINE
 		c = getopt_long(argc, argv, "a:b:hknps:", cmd_opt, &opt_idx);
+#else
+		c = getopt_long(argc, argv, "a:b:e:hknps:", cmd_opt, &opt_idx);
+#endif
 
 		/* Detect the end of the options. */
 		if (c == -1) {
@@ -369,6 +393,12 @@ int main(int argc, char *argv[])
 				exit(1);
 			}
 			break;
+#ifndef OPENSSL_NO_ENGINE
+		case 'e':
+			engine_name = strdup(optarg);
+			use_engine = 1;
+			break;
+#endif
 		case 'h':
 			print_help(argv[0], cmd_opt);
 			exit(0);
@@ -418,6 +448,23 @@ int main(int argc, char *argv[])
 	/* Check command line arguments */
 	check_cmd_params();
 
+
+#ifndef OPENSSL_NO_ENGINE
+	/* Load OpenSSL engine */
+	if (use_engine) {
+		e = init_engine(engine_name);
+		if (e == NULL) {
+			ERROR("Failed to init OpenSSL engine\n");
+			exit(1);
+		}
+		if (atexit(release_engine_atexit)) {
+			ERROR("Failed to register atexit callback\n");
+			release_engine(e);
+			exit(1);
+		}
+	}
+#endif
+
 	/* Indicate SHA as image hash algorithm in the certificate
 	 * extension */
 	if (hash_alg == HASH_ALG_SHA384) {
@@ -440,10 +487,21 @@ int main(int argc, char *argv[])
 		}
 #endif
 
-		/* First try to load the key from disk */
-		if (key_load(&keys[i], &err_code)) {
-			/* Key loaded successfully */
-			continue;
+#ifndef OPENSSL_NO_ENGINE
+		/* Try to load the key using OpenSSL engine */
+		if (use_engine) {
+			if (key_load_using_engine(&keys[i], &err_code, e)) {
+				/* Key loaded successfully */
+				continue;
+			}
+		}
+#endif
+		if (!use_engine) {
+			/* Try to load the key from disk */
+			if (key_load(&keys[i], &err_code)) {
+				/* Key loaded successfully */
+				continue;
+			}
 		}
 
 		/* Key not loaded. Check the error code */
@@ -602,6 +660,9 @@ int main(int argc, char *argv[])
 	key_cleanup();
 
 #ifndef OPENSSL_NO_ENGINE
+	if (e != NULL)
+		release_engine(e);
+
 	ENGINE_cleanup();
 #endif
 	CRYPTO_cleanup_all_ex_data();