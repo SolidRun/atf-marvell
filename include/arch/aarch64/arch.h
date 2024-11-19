Gateworks/atf-newport

cavm: aarch64: Apply WA for errata #30471
When switching to S-EL1/S-EL0, ARM-TF needs to clear
HCR_EL2[BSU].

Do it only for T8x, pass 1.x.

This fixes bugzilla entry #26353

Signed-off-by: Konrad Adamczyk <konrad.adamczyk@cavium.com>

include/lib/aarch64/arch.h
@@ -281,6 +281,8 @@
 #define HCR_AMO_BIT		(U(1) << 5)
 #define HCR_IMO_BIT		(U(1) << 4)
 #define HCR_FMO_BIT		(U(1) << 3)
+#define HCR_BSU0_BIT		(U(1) << 10)
+#define HCR_BSU1_BIT		(U(1) << 11)
 
 /* ISR definitions */
 #define ISR_A_SHIFT		U(8)

SolidRun/atf-marvell

Change-Id: I054a0dc785c6b1191002058aee0c826ad666170c

include/arch/aarch64/arch.h
@@ -613,6 +613,8 @@
 #define HCR_AMO_BIT		(ULL(1) << 5)
 #define HCR_IMO_BIT		(ULL(1) << 4)
 #define HCR_FMO_BIT		(ULL(1) << 3)
+#define HCR_BSU0_BIT		(ULL(1) << 10)
+#define HCR_BSU1_BIT		(ULL(1) << 11)
 
 /* ISR definitions */
 #define ISR_A_SHIFT		U(8)